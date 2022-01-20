38384b1c3b4bc4ca7322a970b51228942e109338
Ji Sangil <gsl0505@naver.com>
2022-01-20 17:15:06 +0900

    first

---
+++ src/Coding.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Coding {
+
+    public int solution(String str,char t){
+        int answer =0;
+        str=str.toUpperCase();
+        t=Character.toUpperCase(t);
+      for(char x : str.toCharArray()){
+
+      }
+      return answer;
+    }
+
+    public static void main(String[] args) {
+        Coding c = new Coding();
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+        char t = s.next().charAt(0);
+        System.out.print(c.solution(str,t));
+    }
+}
