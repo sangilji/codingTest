10c2817efecbc739854b3d0bdd36daddcc3bb432
지상일 <88275989+sangilji@users.noreply.github.com>
2022-02-11 15:45:30 +0900

    2022-02-11

---
+++ src/baekjoon/Surprise.java
@@ -0,0 +1,9 @@
+package baekjoon;
+import java.util.*;
+public class Surprise {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.next() + "??!";
+        System.out.println(a);
+    }
+}
