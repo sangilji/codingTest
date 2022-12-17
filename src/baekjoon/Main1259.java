efe2914b4f08229b2b82df28f704021e15f0e804
sangil <gsl0505@naver.com>
2022-12-17 20:54:13 +0900

    feat(baekjoon): main1259

---
+++ src/baekjoon/Main1259.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main1259 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		while (true) {
+			String x = s.next();
+			if (x.equals("0")) {
+				return;
+			}
+			if (check(x)) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+	}
+
+	private static boolean check(String x) {
+		for (int i = 0; i < x.length() / 2; i++) {
+			if (x.charAt(i) != x.charAt(x.length() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
