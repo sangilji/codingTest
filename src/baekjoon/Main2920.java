c698de2b6ea047c8866d23af4140572a7b8d8506
sangil <gsl0505@naver.com>
2022-12-17 20:45:27 +0900

    feat(baekjoon): main2920

---
+++ src/baekjoon/Main2920.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main2920 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		boolean a = false;
+		boolean b = false;
+		int[] num = new int[8];
+		for (int i = 0; i < 8; i++) {
+			num[i] = s.nextInt();
+
+		}
+		for (int i = 0; i < 7; i++) {
+			if (num[i] < num[i + 1]) {
+				b = true;
+			} else {
+				a = true;
+			}
+		}
+		if (a && b) {
+			System.out.println("mixed");
+		} else if(b){
+			System.out.println("ascending");
+		} else {
+			System.out.println("descending");
+		}
+	}
+}
