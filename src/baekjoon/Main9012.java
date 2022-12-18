5333727eebd4f0d37adb37299b7d135f9f2ee613
sangil <gsl0505@naver.com>
2022-12-18 13:06:25 +0900

    feat(baekjoon): main9012

---
+++ src/baekjoon/Main9012.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main9012 {
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		int t = s.nextInt();
+		for(int i =0; i< t;i++){
+			String str = s.next();
+			check(str);
+		}
+	}
+
+	private static void check(String str) {
+		int count = 0;
+		for(char c : str.toCharArray()){
+			if(c == '('){
+				count++;
+			} else {
+				count --;
+			}
+			if(count<0){
+				System.out.println("NO");
+				return;
+			}
+		}
+		if(count==0){
+			System.out.println("YES");
+		}
+		else{
+			System.out.println("NO");
+		}
+	}
+}
