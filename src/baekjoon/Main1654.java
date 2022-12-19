3e6c52ad77d95bea7142a66d59f1b21b22ebf32b
sangil <gsl0505@naver.com>
2022-12-19 20:43:55 +0900

    feat(baekjoon): main1654

---
+++ src/baekjoon/Main1654.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1654 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int k = s.nextInt();
+		int n = s.nextInt();
+		long[] line = new long[k];
+
+		for (int i = 0; i < k; i++) {
+			line[i] = s.nextInt();
+		}
+		long left = 0;
+		long right = Arrays.stream(line).max().getAsLong() + 1;
+		long mid;
+
+		while (left < right) {
+			long count = 0;
+			mid = (left + right) / 2;
+			for (int i = 0; i < k; i++) {
+				count += (line[i] / mid);
+			}
+			if (count < n) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		System.out.println(left - 1);
+	}
+}
