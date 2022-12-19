3f05752f08bde7ba486b10d01a9206668f8a2eca
sangil <gsl0505@naver.com>
2022-12-19 18:57:25 +0900

    feat(baekjoon): main10816

---
+++ src/baekjoon/Main10816.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main10816 {
+	public static void main(String[] args) throws IOException {
+		Map<String, Integer> map = new HashMap<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st  = new StringTokenizer(br.readLine()," ");
+		for(int i = 0; i< n;i++){
+			String num = st.nextToken();
+			map.put(num,map.getOrDefault(num,0)+1);
+		}
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		for(int i =0;i<m;i++){
+			String num = st.nextToken();
+			sb.append(map.getOrDefault(num,0)).append(" ");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+
+}
