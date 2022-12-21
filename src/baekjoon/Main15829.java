ce10daf31f2b3df5824f52698ed539e6fc9959ab
sangil <gsl0505@naver.com>
2022-12-21 21:21:02 +0900

    feat(baekjoon): main15829

--- src/baekjoon/Main15829.java
+++ src/baekjoon/Main15829.java
@@ -7,11 +7,13 @@ public class Main15829 {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		String a = s.next();
-		long result=0;
+		long result = 0;
+		long pow =1;
 		for (int i = 0; i < n; i++) {
-			int tmp = a.charAt(i)-'a'+1;
-			result+= tmp*Math.pow(31,i);
+			int tmp = a.charAt(i) - 'a' + 1;
+			result+= tmp*pow;
+			pow = (pow*31)%1234567891;
 		}
-		System.out.println(result);
+		System.out.println(result % 1234567891);
 	}
 }
