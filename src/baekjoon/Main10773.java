b4f357ca7bccf01e2ed0f62cf3561074d081fb1f
sangil <gsl0505@naver.com>
2022-12-21 21:08:27 +0900

    feat(baekjoon): main10773

---
+++ src/baekjoon/Main10773.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main10773 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int k = s.nextInt();
+		long sum = 0;
+		Stack<Long> st = new Stack<>();
+		for (int i = 0; i < k; i++) {
+			long n = s.nextLong();
+			if (n == 0) {
+				sum -= st.pop();
+			} else {
+				sum += n;
+				st.add(n);
+			}
+		}
+		System.out.println(sum);
+	}
+}
