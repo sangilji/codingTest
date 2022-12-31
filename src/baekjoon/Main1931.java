1f860af1affd1eb0dc16948de204c2ea80fd7c7d
sangil <gsl0505@naver.com>
2022-12-31 21:48:08 +0900

    feat(baekjoon): main1931

---
+++ src/baekjoon/Main1931.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1931 {
+	static long[][] list;
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		list = new long[n][2];
+		for (int i = 0; i < n; i ++){
+			int a = s.nextInt();
+			int b = s.nextInt();
+			list[i][0] = a;
+			list[i][1] = b;
+		}
+		Arrays.sort(list,(o1, o2) -> {
+			if(o1[1] == o2[1]){
+				return (int)(o1[0] - o2[0]);
+			}
+			return (int)(o1[1] - o2[1]);
+		});
+		long tmp = list[0][1];
+		int result = 1;
+		for (int i =1; i < n ; i++){
+			if(list[i][0] >= tmp){
+				result++;
+				tmp = list[i][1];
+			}
+		}
+		System.out.println(result);
+	}
+
+}
