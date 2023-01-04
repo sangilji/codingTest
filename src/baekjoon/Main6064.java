a94ae155f706f1d1665e91b54c7a12d93596c84e
sangil <gsl0505@naver.com>
2023-01-04 23:16:53 +0900

    feat(baekjoon): Main6064

--- src/baekjoon/Main6064.java
+++ src/baekjoon/Main6064.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class Main6064 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        for (int i = 0; i < t; i++) {
-            int m = s.nextInt();
-            int n = s.nextInt();
-            int x = s.nextInt();
-            int y = s.nextInt();
-            System.out.println(check(m, n, x, y));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
+        for (int s = 0; s < T; s++) {
+            boolean check = false;
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+            for (int i = x; i < (n * m); i += m) {
+                if (i % n == y) {
+                    System.out.println(i + 1);
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) {
+                System.out.println(-1);
+
+            }
         }
     }
 
