dfdc69b31e0566481ca5769d3b1f6e71e612d227
sangil <gsl0505@naver.com>
2023-01-08 17:36:50 +0900

    feat(baekjoon): Main11659

---
+++ src/baekjoon/Main11659.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main11659 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + s.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            System.out.println(dp[b] - dp[a - 1]);
+        }
+
+    }
+}
