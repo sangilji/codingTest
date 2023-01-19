0fcb773e9fe3fc4abb682d3d73b98a9f09f774aa
지상일 <88275989+sangilji@users.noreply.github.com>
2023-01-19 10:44:28 +0900

    [Gold V] Title: 이진 검색 트리, Time: 496 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,56 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static class Node {
-        int num;
-        Node left, right;
-
-        Node(int num) {
-            this.num = num;
+    static StringBuilder sb = new StringBuilder();
+    static class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value){
+            this.value = value;
         }
 
-        Node(int num, Node left, Node right) {
-            this.num = num;
-            this.left = left;
-            this.right = right;
-        }
-
-        void insert(int n) {
-            if (n < this.num) {
-                if (this.left == null)
-                    this.left = new Node(n);
-                else this.left.insert(n);
+        void add(int tmp) {
+            if (this.value>tmp){
+                if (this.left != null){
+                    this.left.add(tmp);
+                } else {
+                    this.left = new Node(tmp);
+                }
             } else {
-                if (this.right == null)
-                    this.right = new Node(n);
-                else this.right.insert(n);
+                if (this.right != null){
+                    this.right.add(tmp);
+                } else {
+                    this.right = new Node(tmp);
+                }
             }
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Node root = new Node(Integer.parseInt(br.readLine()));
-        String input;
-        while (true) {
-            input = br.readLine();
-            if (input == null || input.equals(""))
+        Node startNode = new Node(Integer.parseInt(br.readLine()));
+        String command;
+        while (true){
+            command = br.readLine();
+            if(command==null|| command.equals("")){
                 break;
-            root.insert(Integer.parseInt(input));
-        }
+            }
+            int tmp = Integer.parseInt(command);
 
-        postOrder(root);
+            startNode.add(tmp);
+        }
+        print(startNode);
+        System.out.println(sb);
     }
 
-    static void postOrder(Node node) {
-        if (node == null)
-            return;
 
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.num);
+    private static void print(Node current) {
+        if (current==null){
+            return;
+        }
+        if (current.left!=null) {
+            print(current.left);
+        }
+        if (current.right!=null){
+            print(current.right);
+        }
+        sb.append(current.value).append("\n");
     }
 }
