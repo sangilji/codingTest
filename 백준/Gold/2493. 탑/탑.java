a29844b7aeeae179620d482eaaa45dc8f54e1f20
지상일 <88275989+sangilji@users.noreply.github.com>
2023-02-13 15:12:17 +0900

    [Gold V] Title: 탑, Time: 884 ms, Memory: 111568 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,33 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         Stack<int[]> s = new Stack<>();
-        for(int i = 0;i<a;i++){
-            int[] tmp = {i,Integer.parseInt(st.nextToken())};
-            if(s.empty()) {
-                s.add(tmp);
-                System.out.print(0 + " ");
-            }
-            else {
-                while (s.peek()[1] < tmp[1]) {
-                    s.pop();
-                    if(s.empty())
-                        break;
-
+        String[] r = new String[n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.isEmpty()) {
+                s.add(new int[]{arr[i],i});
+            } else {
+                while (!s.isEmpty() && s.peek()[0] < arr[i]) {
+                    int[] tmp = s.pop();
+                    r[tmp[1]] = String.valueOf(i+1);
                 }
-                if(s.empty())
-                    System.out.print(0+ " ");
-                else
-                    System.out.print(s.peek()[0]+1+" ");
-                s.add(tmp);
-
+                s.add(new int[]{arr[i],i});
             }
         }
+        while (!s.isEmpty()) {
+            int[] tmp = s.pop();
+            r[tmp[1]] = String.valueOf(0);
+        }
 
+        System.out.println(String.join(" ",r));
     }
 }
