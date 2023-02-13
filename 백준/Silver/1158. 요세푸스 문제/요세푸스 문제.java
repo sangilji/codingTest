f9d4f07c658785ba250f9d81aea148382868038b
지상일 <88275989+sangilji@users.noreply.github.com>
2023-02-13 14:18:21 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 152 ms, Memory: 15048 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,21 +1,27 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c=-1;
-        ArrayList<Integer> arr =new ArrayList<>();
-        System.out.print("<");
-        for(int i =1;i<=a;i++)
-            arr.add(i);
-        while(arr.size()!=1) {
-            c = (c + b) % arr.size();
-            System.out.print(arr.get(c) + ", ");
-            arr.remove(c);
-            c--;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        List<String> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            arr.add(String.valueOf(i));
         }
-        System.out.print(arr.get(0)+">");
+
+        int start = -1;
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            start = (start+k) % (n-i);
+            result.add(arr.remove(start));
+            start--;
+        }
+        System.out.printf("<%s>%n", String.join(", ", result));
+
     }
 }
