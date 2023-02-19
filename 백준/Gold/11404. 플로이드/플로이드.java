84bcfbfb05dbc222a2a5e53551c54da8a47b88ef
지상일 <88275989+sangilji@users.noreply.github.com>
2023-02-19 19:45:23 +0900

    [Gold IV] Title: 플로이드, Time: 428 ms, Memory: 43160 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,47 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static final int INF = 10000000;
+    private static final int INF = 10000000;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int m = s.nextInt();
-		int[][] costs = new int[n + 1][n + 1];
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (i == j) {
-					costs[i][j] = 0;
-				} else {
-					costs[i][j] = INF;
-				}
-			}
-		}
-		for (int i = 0; i < m; i++) {
-			int a = s.nextInt();
-			int b = s.nextInt();
-			int cost = s.nextInt();
-			costs[a][b] = Math.min(costs[a][b], cost);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] costs = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    costs[i][j] = 0;
+                } else {
+                    costs[i][j] = INF;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            costs[a][b] = Math.min(costs[a][b], cost);
+        }
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				for (int k = 1; k <= n; k++) {
-					costs[j][k] = Math.min(costs[j][k], costs[j][i] + costs[i][k]);
-				}
-			}
-		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (costs[i][j] == INF) {
-					System.out.print("0 ");
-				} else {
-					System.out.print(costs[i][j] + " ");
-				}
-			}
-			System.out.println();
-		}
-	}
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    costs[j][k] = Math.min(costs[j][k], costs[j][i] + costs[i][k]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (costs[i][j] == INF) {
+                    sb.append("0 ");
+                } else {
+                    sb.append(costs[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
