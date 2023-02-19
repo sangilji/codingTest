ecabf4ef001c1ab5fdce62b85380ec4d88306dcd
지상일 <88275989+sangilji@users.noreply.github.com>
2023-02-19 19:38:56 +0900

    [Gold IV] Title: 최단경로, Time: 828 ms, Memory: 107896 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,75 +1,78 @@
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static class Node {
-		int last;
-		int cost;
+    static class Node {
+        int last;
+        int cost;
 
-		public Node(int last, int cost) {
-			this.last = last;
-			this.cost = cost;
-		}
-	}
+        public Node(int last, int cost) {
+            this.last = last;
+            this.cost = cost;
+        }
+    }
 
-	static int[] max;
-	static int K;
-	static List<List<Node>> nodes = new ArrayList<>();
+    static int K;
+    static List<List<Node>> nodes = new ArrayList<>();
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
 
-		int V = s.nextInt();
-		int E = s.nextInt();
-		K = s.nextInt();
-		max = new int[V + 1];
-		Arrays.fill(max, Integer.MAX_VALUE);
-		max[K] = 0;
-		for (int i = 0; i < V + 1; i++) {
-			nodes.add(new ArrayList<>());
-		}
-		for (int i = 0; i < E; i++) {
-			int u = s.nextInt();
-			int v = s.nextInt();
-			int w = s.nextInt();
-			nodes.get(u).add(new Node(v, w));
-		}
+        for (int i = 0; i < V + 1; i++) {
+            nodes.add(new ArrayList<>());
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            nodes.get(u).add(new Node(v, w));
+        }
 
-		bfs();
-		for (int i = 1; i < max.length; i++) {
-			if (max[i] == Integer.MAX_VALUE) {
-				System.out.println("INF");
-				continue;
-			}
-			System.out.println(max[i]);
+        bfs();
 
-		}
+    }
 
-	}
+    public static void bfs() {
+        int[] dist = new int[nodes.size()];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        boolean[] visit = new boolean[nodes.size()];
+        PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+        q.add(new Node(K, 0));
+        dist[K] = 0;
 
-	public static void bfs() {
-		PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
-		q.add(new Node(K, 0));
+        while (!q.isEmpty()) {
+            int tmp = q.poll().last;
 
-		while (!q.isEmpty()) {
-			Node tmp = q.poll();
-			if (max[tmp.last] < tmp.cost) {
-				continue;
-			}
-			for (int i = 0; i < nodes.get(tmp.last).size(); i++) {
-				Node currentNode = nodes.get(tmp.last).get(i);
-				if(max[currentNode.last] > currentNode.cost + tmp.cost){
-					max[currentNode.last] = currentNode.cost + tmp.cost;
-					q.add(new Node(currentNode.last, max[currentNode.last]));
-				}
+            if (visit[tmp]){
+                continue;
+            }
+            visit[tmp]=true;
+            for (int i = 0; i < nodes.get(tmp).size(); i++) {
+                Node currentNode = nodes.get(tmp).get(i);
 
-			}
-		}
-	}
+                if(dist[currentNode.last] > currentNode.cost + dist[tmp]){
+                    dist[currentNode.last] = currentNode.cost + dist[tmp];
+                    q.add(new Node(currentNode.last, dist[currentNode.last]));
+                }
+
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < nodes.size(); i++) {
+            if (dist[i] == Integer.MAX_VALUE){
+                sb.append("INF\n");
+            }else {
+                sb.append(dist[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
-
