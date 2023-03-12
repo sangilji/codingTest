e799f96c58a1e1e01a6ce2ee8c0743d0747e5aeb
지상일 <gsl0505@naver.com>
2023-03-12 11:53:02 +0900

    feat(Main26711): a + b

--- src/baekjoon/Main26711.java
+++ src/baekjoon/Main26711.java
@@ -3,35 +3,22 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main26711 {
     public static void main(String[] args) throws IOException {
         //코드를 작성하세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        String b = br.readLine();
-        int maxLen = Math.max(a.length(), b.length());
-        int[] A = new int[maxLen + 1];
-        int[] B = new int[maxLen + 1];
-
-        for (int i = a.length() - 1, idx = 0; i >= 0; i--, idx++) {
-            A[idx] = a.charAt(i) - '0';
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
         }
-        for (int i = b.length() - 1, idx = 0; i >= 0; i--, idx++) {
-            B[idx] = b.charAt(i) - '0';
+        Arrays.sort(arr, (o1, o2) -> {
+            return o2.length() - o1.length();
+        });
+        for (String s : arr) {
+            System.out.println(s);
         }
-        for (int i = 0; i < maxLen; i++) {
-            int value = A[i] + B[i];
-            A[i] = value % 10;
-            A[i + 1] = A[i + 1] + (value / 10);
-        }
-        StringBuilder sb = new StringBuilder();
-        if (A[maxLen] != 0) {
-            sb.append(A[maxLen]);
-        }
-        for (int i = maxLen - 1; i >= 0; i--) {
-            sb.append(A[i]);
-        }
-        System.out.println(sb);
     }
 }
