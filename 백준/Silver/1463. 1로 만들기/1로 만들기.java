968c549cd3fa07653cff31e0edefec663afdc211
지상일 <88275989+sangilji@users.noreply.github.com>
2023-03-28 14:10:59 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 15572 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[] dp = new int[1000001];
-        dp[2]=1;
-        dp[3]=1;
-        for(int i =4;i<=n;i++){
-            dp[i]= dp[i-1]+1;
-            if(i%3==0){
-                dp[i]=Math.min(dp[i/3]+1,dp[i]);
-            } 
-            if(i%2==0){
-                dp[i]=Math.min(dp[i/2]+1,dp[i]);
+    static boolean[] visit = new boolean[1000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        bfs(n);
+
+    }
+
+    private static void bfs(int n) {
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o->o[1]));
+        q.add(new int[]{n, 0});
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            if (visit[tmp[0]]) {
+                continue;
             }
+            visit[tmp[0]]=true;
+
+            if (tmp[0]==1){
+                System.out.println(tmp[1]);
+                return;
+            }
+            if (tmp[0] % 3 == 0) {
+                q.add(new int[]{tmp[0]/3, tmp[1]+1});
+            }
+            if (tmp[0] % 2 == 0) {
+                q.add(new int[]{tmp[0]/2, tmp[1]+1});
+            }
+            q.add(new int[]{tmp[0]-1, tmp[1]+1});
         }
-        System.out.println(dp[n]);
     }
 }
