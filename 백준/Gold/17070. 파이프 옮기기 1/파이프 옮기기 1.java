76249da0f24a7d80e7e19dcb126fa012ee4c78a4
지상일 <88275989+sangilji@users.noreply.github.com>
2023-03-29 14:13:09 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -5,78 +5,45 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int[][] map;
-    static int count = 0;
     static int[] dx = {0, 1, 1};
     static int[] dy = {1, 1, 0};
 
     public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+        map = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < n + 1; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dfs(0, 1, n - 1, 0);
-        System.out.println(count);
-    }
+        int[][][] mapDis = new int[n + 1][n + 1][3];
+        mapDis[1][2][0] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int x = dx[k] + j;
+                    int y = dy[k] + i;
 
-    private static void dfs(int x, int y, int n, int shape) {
-        if (x == n && y == n) {
-            count++;
-            return;
-        }
-        if (shape == 0) {
-            for (int i = 0; i < 2; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (i == 1) {
-                    if (tx > n || ty > n || map[tx][ty] == 1 || map[tx - 1][ty] == 1 || map[tx][ty - 1] == 1) {
+                    if (y > n || x > n || map[x][y] == 1) {
                         continue;
                     }
-                } else {
-                    if (tx > n || ty > n || map[tx][ty] == 1) {
-                        continue;
+                    if (k == 0) {
+                        mapDis[x][y][0] = mapDis[j][i][0] + mapDis[j][i][1];
+                    } else if (k == 1) {
+                        if (map[x - 1][y] == 0 && map[x][y - 1] == 0) {
+                            mapDis[x][y][1] = mapDis[j][i][1] + mapDis[j][i][0] + mapDis[j][i][2];
+                        }
+                    } else {
+                        mapDis[x][y][2] = mapDis[j][i][1] + mapDis[j][i][2];
                     }
                 }
-                dfs(tx, ty, n, i);
-            }
-        } else if (shape == 1) {
-            for (int i = 0; i < 3; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (i == 1) {
-                    if (tx > n || ty > n || map[tx][ty] == 1 || map[tx - 1][ty] == 1 || map[tx][ty - 1] == 1) {
-                        continue;
-                    }
-                } else {
-                    if (tx > n || ty > n || map[tx][ty] == 1) {
-                        continue;
-                    }
-                }
-                dfs(tx, ty, n, i);
-            }
-        } else {
-            for (int i = 1; i < 3; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (i == 1) {
-                    if (tx > n || ty > n || map[tx][ty] == 1 || map[tx - 1][ty] == 1 || map[tx][ty - 1] == 1) {
-                        continue;
-                    }
-                } else {
-                    if (tx > n || ty > n || map[tx][ty] == 1) {
-                        continue;
-                    }
-                }
-                dfs(tx, ty, n, i);
             }
         }
-    }
 
+
+        System.out.print(mapDis[n][n][0] + mapDis[n][n][1] + mapDis[n][n][2]);
+           
+    }
 }
