dd64bfa0cb0e3d9bd660eda008bf031ac10950e8
지상일 <88275989+sangilji@users.noreply.github.com>
2023-03-30 14:38:17 +0900

    [Gold IV] Title: 연구소, Time: 416 ms, Memory: 105464 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,82 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
-	private static int M;
-	private static int[][] map;
-	private static int[] dx = {1, -1, 0, 0};
-	private static int[] dy = {0, 0, 1, -1};
-	static Queue<int[]> q = new LinkedList<>();
-	static int max = 0;
+    static int n;
+    static int m;
+    static int[][] map;
+    static int[][] wall = new int[3][2];
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int max = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dfs(0,0);
+        System.out.println(max);
+    }
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		N = s.nextInt();
-		M = s.nextInt();
-		map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				map[i][j] = s.nextInt();
-			}
-		}
-		dfs(0);
+    private static void dfs(int depth,int current) {
+        if (depth == 3) {
+            check();
+            return;
+        }
+        while (n*m>current) {
+            int x = current / m;
+            int y = current % m;
+            if (map[x][y] == 0) {
+                wall[depth][0] = x;
+                wall[depth][1] = y;
+                dfs(depth + 1, current + 1);
+            }
+            current++;
+        }
 
-		System.out.println(max);
-	}
+    }
 
-	private static void dfs(int depth) {
-		if (depth == 3) {
-			result();
-			return;
-		}
+    private static void check() {
+        int[][] tmpMap = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            tmpMap[i] = map[i].clone();
+        }
+        for (int i = 0; i < 3; i++) {
+            tmpMap[wall[i][0]][wall[i][1]] = 1;
+        }
+        
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] > 0) {
-					continue;
-				}
-				map[i][j] = 1;
-				dfs(depth + 1);
-				map[i][j] = 0;
-			}
-		}
-	}
-
-	private static void result() {
-		int[][] virus_map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				virus_map[i][j] = map[i][j];
-				if(map[i][j]==2){
-					q.add(new int[]{i,j});
-				}
-			}
-		}
-
-		while (!q.isEmpty()) {
-			int[] pos = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int x = pos[0] + dx[i];
-				int y = pos[1] + dy[i];
-				if (x < 0 || y < 0 || x >= N || y >= M || virus_map[x][y]>0) {
-					continue;
-				}
-				virus_map[x][y] = 2;
-				q.add(new int[]{x, y});
-			}
-		}
-		maxValue(virus_map);
-	}
-
-	private static void maxValue(int[][] virus_map) {
-
-		max = Math.max(max, Arrays.stream(virus_map)
-			.mapToInt(n -> (int)Arrays.stream(n)
-				.filter(m -> m == 0)
-				.count())
-			.sum());
-	}
+        Queue<int[]> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmpMap[i][j] == 2) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int x = dx[i] + tmp[0];
+                int y = dy[i] + tmp[1];
+                if (x < 0 || y < 0 || x >= n || y >= m || tmpMap[x][y] != 0) {
+                    continue;
+                }
+                tmpMap[x][y]=2;
+                q.add(new int[]{x, y});
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmpMap[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        
+        max = Math.max(count, max);
+    }
 }
