02a3301e4542afe57715f1ea9ce43c7c22041cf7
지상일 <88275989+sangilji@users.noreply.github.com>
2023-04-04 11:53:17 +0900

    [Platinum V] Title: 찾기, Time: 2880 ms, Memory: 92076 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+// KMP 알고리즘(Knuth–Morris–Pratt Algorithm)
+// O(N+M)
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        char[] text = in.readLine().toCharArray();
+        char[] pattern = in.readLine().toCharArray();
+
+        int tLength = text.length, pLength = pattern.length;
+
+        int[] pi = new int[pLength];
+        for (int i = 1, j = 0; i < pLength; i++) {
+            while (j > 0 && pattern[i] != pattern[j]) j = pi[j - 1];
+
+            if (pattern[i] == pattern[j]) pi[i] = ++j;
+            else pi[i] = 0;
+        }
+
+        int cnt = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // i : 텍스트 포인터 , j: 패턴 포인터
+        for (int i = 0, j = 0; i < tLength; ++i) {
+
+            while (j > 0 && text[i] != pattern[j]) j = pi[j - 1];
+
+            if (text[i] == pattern[j]) { //두 글자 일치
+                if (j == pLength - 1) { // j가 패턴의 마지막 인덱스라면
+                    cnt++; // 카운트 증가
+                    list.add(i - j);
+                    j = pi[j];
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print((list.get(i)+1) +" ");
+        }
+    }
+}
