acb911c4989cba545ddda80b791eb24f2aafb4d3
지상일 <88275989+sangilji@users.noreply.github.com>
2023-04-05 17:47:41 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
