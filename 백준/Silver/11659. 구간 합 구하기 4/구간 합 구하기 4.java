143c6626ef9859b29743651d40cfa9083cd622ec
지상일 <88275989+sangilji@users.noreply.github.com>
2023-04-07 11:42:05 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 684 ms, Memory: 56008 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] tree;
+static int n;
     public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        int[] map = new int[n + 1];
+        int[] arr = new int[n + 1];
+        tree = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-        for (int j = 1; j < n + 1; j++) {
-            map[j] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int j = 1; j < n + 1; j++) {
-            map[j] += map[j - 1];
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            add(i, arr[i]);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -26,12 +25,24 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            int sum = map[b] - map[a-1];
-
-            sb.append(sum).append("\n");
-
+            sb.append(sum(b) - sum(a - 1)).append("\n");
         }
         System.out.println(sb);
+    }
+
+    public static void add(int i, int num) {
+        while (i <= n) {
+            tree[i] += num;
+            i += (i & -i);
+        }
+    }
+
+    public static int sum(int i) {
+        int tmp = 0;
+        while (i > 0) {
+            tmp += tree[i];
+            i -= (i & -i);
+        }
+        return tmp;
     }
 }
