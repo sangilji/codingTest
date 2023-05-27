7cc6748820dc03960eedba27d44fd0ec9127cdbf
지상일 <88275989+sangilji@users.noreply.github.com>
2023-05-27 22:41:09 +0900

    [Silver IV] Title: 다음 소수, Time: 948 ms, Memory: 110400 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	static boolean prime[] = new boolean[10004002];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			long num = Long.parseLong(br.readLine());
+			var bi = new BigInteger(String.valueOf(num));
+
+			if (bi.isProbablePrime(10)) {
+				System.out.println(num);
+			} else {
+				System.out.println(bi.nextProbablePrime());
+			}
+		}
+
+		br.close();
+	}
+}
