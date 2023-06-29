b3016edaa049c9a1c81b88cdf209150edda6d8e7
지상일 <88275989+sangilji@users.noreply.github.com>
2023-06-29 22:29:59 +0900

    [Bronze IV] Title: Hello Judge, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1 ; i <= n ; i++){
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+        System.out.println(sb);
+    }
+}
