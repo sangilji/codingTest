517487fc072f25d031e3ba7e4175fa1f2759a319
지상일 <88275989+sangilji@users.noreply.github.com>
2023-07-14 22:34:08 +0900

    [Gold V] Title: 퇴사 2, Time: 900 ms, Memory: 332908 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -5,39 +5,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int[][] arr;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n + 1][2];
-        int[][] arr = new int[n + 1][2];
-        for (int i = 1; i < n + 1; i++) {
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][2];
+        dp = new int[n + 100];
+        Arrays.fill(dp, -941841);
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 2; j++) {
-
-                dp[i][j] = Integer.parseInt(st.nextToken());
-                arr[i][j] = dp[i][j];
-                if (arr[i][0] + i > n + 1) {
-                    dp[i][1] = 0;
-
-                }
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int max = 0;
-        for (int i = 1; i < n + 1; i++) {
-            int tmp = dp[i][0];
-            int tmp1 = dp[Math.min(i + tmp, n)][0];
-            for (int j = i + tmp; j <= i + tmp + tmp1; j++) {
-                if (j > n) {
-                    break;
-                }
-                if (dp[j][0] + j <= n + 1) {
-                    dp[j][1] = Math.max(dp[j][1], arr[j][1] + dp[i][1]);
-                }
-
-            }
-            max = Math.max(max, dp[i][1]);
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(dp[i + 1], dp[i + arr[i][0]] + arr[i][1]);
         }
-        System.out.println(max);
+        System.out.println(dp[0]);
+
     }
+
+    private static int find(int depth) {
+        if (depth > n) {
+            return -1393145;
+        }
+        if (n == depth) {
+            return 0;
+        }
+        if (dp[depth] != -1) {
+            return dp[depth];
+        }
+
+        return dp[depth] = Math.max(find(depth + 1), find(depth + arr[depth][0]) + arr[depth][1]);
+
+
+    }
+
+
 }
