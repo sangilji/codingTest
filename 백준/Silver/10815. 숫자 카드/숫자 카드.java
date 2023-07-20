c5b3a0815faecc40bac31a6227c7e014b445b932
지상일 <88275989+sangilji@users.noreply.github.com>
2023-07-20 22:56:55 +0900

    [Silver V] Title: 숫자 카드, Time: 948 ms, Memory: 136928 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,49 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] card;
 	static int n;
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		card = new int[n];
+		Set<Integer> card = new HashSet<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
+			card.add(Integer.parseInt(st.nextToken()));
 		}
-		Arrays.sort(card);
 		int a = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < a; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			search(num);
-		}
-		System.out.println(sb);
-	}
-
-	private static void search(int num) {
-		int left = 0;
-		int right = n - 1;
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			int m = card[mid];
-
-			if (num < m) {
-				right = mid - 1;
-			} else if (num > m) {
-				left = mid + 1;
-			} else {
+			if (card.contains(num)) {
 				sb.append("1 ");
-				return;
+			} else {
+				sb.append("0 ");
 			}
 		}
-		sb.append("0 ");
-		return;
+		System.out.println(sb);
 	}
 }
