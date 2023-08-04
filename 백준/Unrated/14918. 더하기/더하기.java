d3fd1eeeb20ab1b8d448f9fb20aa1ae99e14cebf
지상일 <88275989+sangilji@users.noreply.github.com>
2023-08-04 22:15:12 +0900

    [Unrated] Title: 더하기, Time: 204 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println((a+b));    
+    }
+}
