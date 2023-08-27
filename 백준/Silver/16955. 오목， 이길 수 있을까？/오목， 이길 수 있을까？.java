c0e7b3f80971eddff8128d315f671ef25051748a
지상일 <88275989+sangilji@users.noreply.github.com>
2023-08-27 20:16:31 +0900

    [Silver III] Title: 오목, 이길 수 있을까?, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.java
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.java
@@ -32,42 +32,81 @@ public class Main {
 
 	}
 
-	private static boolean check(int r, int c) {
-		// horizon chk
-		int cnt = 0;
-		for (int i = c - 1; i >= 0 && arr[r][i] == 'X'; i--)
-			cnt++;
-		for (int i = c + 1; i < 10 && arr[r][i] == 'X'; i++)
-			cnt++;
-		if (cnt >= 4)
+	private static boolean check(int i, int j) {
+		int count = 1;
+		for (int k = j - 1; k >= 0; k--) {
+			if (arr[i][k] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		for (int k = j + 1; k < 10; k++) {
+			if (arr[i][k] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		if (count >= 5) {
 			return true;
+		}
+		count = 1;
+		for (int k = i - 1; k >= 0; k--) {
+			if (arr[k][j] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		for (int k = i + 1; k < 10; k++) {
+			if (arr[k][j] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		if (count >= 5) {
+			return true;
+		}
+		count = 1;
 
-		// vertical chk
-		cnt = 0;
-		for (int i = r - 1; i >= 0 && arr[i][c] == 'X'; i--)
-			cnt++;
-		for (int i = r + 1; i < 10 && arr[i][c] == 'X'; i++)
-			cnt++;
-		if (cnt >= 4)
+		for (int k = i - 1, l = j - 1; k >= 0 && l >= 0; k--, l--) {
+			if (arr[k][l] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		for (int k = i + 1, l = j + 1; k < 10 && l < 10; k++, l++) {
+			if (arr[k][l] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		if (count >= 5) {
 			return true;
+		}
 
-		// '/' diagonal chk
-		cnt = 0;
-		for (int i = r + 1, j = c - 1; i < 10 && j >= 0 && arr[i][j] == 'X'; i++, j--)
-			cnt++;
-		for (int i = r - 1, j = c + 1; i >= 0 && j < 10 && arr[i][j] == 'X'; i--, j++)
-			cnt++;
-		if (cnt >= 4)
+		count = 1;
+		for (int k = i - 1, l = j + 1; k >= 0 && l < 10; k--, l++) {
+			if (arr[k][l] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		for (int k = i + 1, l = j - 1; k < 10 && l >= 0; k++, l--) {
+			if (arr[k][l] == 'X') {
+				count++;
+			} else {
+				break;
+			}
+		}
+		if (count >= 5) {
 			return true;
-
-		// '\' diagonal chk
-		cnt = 0;
-		for (int i = r + 1, j = c + 1; i < 10 && j < 10 && arr[i][j] == 'X'; i++, j++)
-			cnt++;
-		for (int i = r - 1, j = c - 1; i >= 0 && j >= 0 && arr[i][j] == 'X'; i--, j--)
-			cnt++;
-		if (cnt >= 4)
-			return true;
+		}
 
 		return false;
 	}
