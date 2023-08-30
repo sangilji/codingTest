5a173649f6da022da24712285712f8b1c409d24a
지상일 <88275989+sangilji@users.noreply.github.com>
2023-08-30 15:06:12 +0900

    [Bronze I] Title: 선 그리기, Time: 304 ms, Memory: 19368 KB -BaekjoonHub

---
+++ 백준/Bronze/16396. 선 그리기/선 그리기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[10001];
+		int n = Integer.parseInt(br.readLine());
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			for (int j = a; j < b; j++) {
+				if (arr[j] > 0) {
+					continue;
+				}
+				arr[j]++;
+				count++;
+			}
+		}
+		System.out.println(count);
+
+
+	}
+
+
+
+}
