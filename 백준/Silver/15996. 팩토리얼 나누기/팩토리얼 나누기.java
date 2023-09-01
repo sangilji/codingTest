0238b61fd3689f9936caaeb33ac1ee0f70455881
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-01 21:10:55 +0900

    [Silver III] Title: 팩토리얼 나누기, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/15996. 팩토리얼 나누기/팩토리얼 나누기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int MOD = 1_000_000;
+	static int n;
+	static int m;
+	static Map<Integer, Integer>[] list;
+	static Map<Integer, Integer> map = new HashMap<>();
+	static long[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int count = 0;
+		while (n >= 1) {
+			count += n / m;
+			n /= m;
+		}
+		System.out.println(count);
+
+	}
+
+	private static long gcd(long a, long b) {
+		if (a > b) {
+			long tmp = a;
+			a = b;
+			b = tmp;
+		}
+		if (a == 0) {
+			return b;
+		}
+		while (true) {
+			if (b % a == 0) {
+				return a;
+			}
+			b %= a;
+
+			long tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+	}
+
+}
