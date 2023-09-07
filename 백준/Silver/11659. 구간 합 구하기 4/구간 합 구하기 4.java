4063702114be45f5c85c5bc822e55bd4ec3d381c
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-07 16:04:23 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 560 ms, Memory: 54656 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,48 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] tree;
-static int n;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n + 1];
-        tree = new int[n + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            add(i, arr[i]);
-        }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(sum(b) - sum(a - 1)).append("\n");
-        }
-        System.out.println(sb);
-    }
+	static int MOD = 1_000_000;
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 150_000;
 
-    public static void add(int i, int num) {
-        while (i <= n) {
-            tree[i] += num;
-            i += (i & -i);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+		}
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(arr[b] - arr[a - 1]).append("\n");
+		}
+		System.out.println(sb);
 
-    public static int sum(int i) {
-        int tmp = 0;
-        while (i > 0) {
-            tmp += tree[i];
-            i -= (i & -i);
-        }
-        return tmp;
-    }
+	}
+
 }
