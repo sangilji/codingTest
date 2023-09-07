416c8a0edf327d2af013d1a9aff95f92f070a90d
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-07 13:24:49 +0900

    [Silver II] Title: 연속합, Time: 292 ms, Memory: 24020 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,20 +1,47 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[] arr = new int[n];
-        for(int i =0;i<n;i++){
-            arr[i]=s.nextInt();
-        }
-        int sum=arr[0];
-        int max=arr[0];
-        for(int i =1;i<n;i++){
-            sum=Math.max(sum+arr[i],arr[i]);
-            max = Math.max(max, sum);
-        }
-        System.out.println(max+"");
-    }
+
+	static int MOD = 1_000_000;
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 1_000_000;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		long max = -10000;
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+
+		}
+		int[] prefix_min = new int[n + 1];
+		prefix_min[0] = 2000;
+		for (int i = 1; i <= n; i++) {
+			prefix_min[i] = Math.min(arr[i-1],prefix_min[i - 1]);
+		}
+
+		for (int i = 1; i <= n; i++) {
+			max = Math.max(arr[i] - prefix_min[i],max);
+		}
+		System.out.println(max);
+	}
+
 }
