277d6788d27b3c541adb30e4d382cdbf2812533a
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-08 13:18:50 +0900

    [Gold II] Title: 최대공약수 하나 빼기, Time: 604 ms, Memory: 138076 KB -BaekjoonHub

--- 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.java
+++ 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.java
@@ -1,75 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
 
+	static int MOD = 1_000_000_007;
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Long, Integer> map = new HashMap<>();
+	static long[] prefix_gcd;
+	static long[] suffix_gcd;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 150_000;
+	static String s;
+	static int result = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		long[] arr = new long[n + 1];
+		prefix_gcd = new long[n + 2];
+		suffix_gcd = new long[n + 2];
 
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			prefix_gcd[i] = gcd(prefix_gcd[i - 1], arr[i]);
+		}
+		for (int i = n; i >= 1; i--) {
+			suffix_gcd[i] = gcd(suffix_gcd[i + 1], arr[i]);
+		}
+		long answer = -1;
+		int index = -1;
+		for (int i = 1; i <= n; i++) {
+			long result = gcd(prefix_gcd[i - 1], suffix_gcd[i + 1]);
+			if (arr[i] % result == 0) {
+				continue;
+			}
+			if (answer<result) {
+				answer = result;
+				index = i;
+			}
+		}
+		if (answer == -1) {
+			System.out.println(answer);
+			return;
+		}
+		sb.append(answer).append(" ").append(arr[index]);
+		System.out.println(sb);
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int[][] dp = new int[n][2];
-        for (int i = 1; i < n; i++) {
-            if (dp[i - 1][0] == 0) {
-                dp[i][0] = arr[i - 1];
-            } else {
-                dp[i][0] = gcd(dp[i - 1][0], arr[i - 1]);
-            }
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            if (dp[i + 1][1] == 0) {
-                dp[i][1] = arr[i + 1];
-            } else {
-                dp[i][1] = gcd(dp[i + 1][1], arr[i + 1]);
-            }
-        }
-        int max = -1;
-        int value = -1;
-        for (int i = 0; i < n; i++) {
-            int gcd = gcd(dp[i][0], dp[i][1]);
-            if (arr[i] % gcd == 0) {
-                continue;
-            }
-            if (max < gcd) {
-                max = gcd;
-                value = arr[i];
-            }
-        }
-        if (max != -1) {
-            System.out.println(max + " " + value);
-        } else {
-            System.out.println(-1);
-        }
-    }
+	}
 
-    private static int gcd(int a, int b) {
-        if (a > b) {
-            int tmp = a;
-            a = b;
-            b = tmp;
-        }
-        if (a == 0) {
-            return b;
-        }
-        while (true) {
-            if (b % a == 0) {
-                return a;
-            }
-            b = b % a;
-            int tmp = a;
-            a = b;
-            b = tmp;
-        }
-    }
+	private static long gcd(long a, long b) {
+		if (a > b) {
+			long tmp = a;
+			a = b;
+			b = tmp;
+		}
+		if (a == 0) {
+			return b;
+		}
+		while (true) {
 
+			if (b % a == 0) {
+				return a;
+			}
+			b %= a;
+			long tmp = a;
+			a = b;
+			b = tmp;
+
+		}
+
+	}
+/*
+3
+3 1 2
+2 1 4
+ */
 
 }
