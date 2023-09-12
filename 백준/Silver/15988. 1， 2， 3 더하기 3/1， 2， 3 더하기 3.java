093b030fbedf904a3c66ffb91beaf5a0953a3fd3
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-12 23:08:11 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 176 ms, Memory: 18360 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -11,9 +11,14 @@ public class Main {
 	static int MOD = 1_000_000_009;
 	static int n;
 	static int m;
-	static List<Integer> list = new ArrayList<>();
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
 	static Map<Integer, Integer> map = new HashMap<>();
-	static long[] arr;
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 100_000;
 	static int[] dp;
 
 	public static void main(String[] args) throws IOException {
@@ -22,80 +27,20 @@ public class Main {
 		int t = Integer.parseInt(br.readLine());
 		n = 1000000;
 		dp = new int[n + 1];
-		dp[n] = 1;
-		for (int j = n - 1; j >= 0; j--) {
+		dp[0] = 1;
+		for (int j = 0; j <= n; j++) {
 			for (int k = 1; k <= 3; k++) {
-				if (j + k <= n) {
-					dp[j] = (dp[j] + dp[j + k]) % MOD;
+				if (j - k >= 0) {
+					dp[j] = (dp[j] + dp[j - k]) % MOD;
 				}
 			}
 		}
 		for (int i = 0; i < t; i++) {
 			n = Integer.parseInt(br.readLine());
+			sb.append(dp[n]).append("\n");
 
-			sb.append(dp[1000000 - n]).append("\n");
 		}
 		System.out.println(sb);
-
-	}
-
-	private static boolean find(int i, int c) {
-		int div = 10;
-		while (i >= 1) {
-			if (i % div == c) {
-				return true;
-			}
-			i /= 10;
-		}
-		return false;
-	}
-
-	private static int recur(int depth) {
-		if (depth > n) {
-			return 0;
-		}
-		if (depth == n) {
-			return 1;
-		}
-		if (dp[depth] != -1) {
-			return dp[depth];
-		}
-		dp[depth] = ((recur(depth + 1) + recur(depth + 2)) % MOD + recur(depth + 3)) % MOD;
-		return dp[depth];
-	}
-
-	private static int[] findPrime() {
-		int[] prime = new int[4_000_010];
-		for (int i = 2; i <= Math.sqrt(4_000_010); i++) {
-			if (prime[i] == 1) {
-				continue;
-			}
-			for (int j = i + i; j < 4_000_010; j += i) {
-				prime[j] = 1;
-			}
-		}
-		return prime;
-	}
-
-	private static long gcd(long a, long b) {
-		if (a > b) {
-			long tmp = a;
-			a = b;
-			b = tmp;
-		}
-		if (a == 0) {
-			return b;
-		}
-		while (true) {
-			if (b % a == 0) {
-				return a;
-			}
-			b %= a;
-
-			long tmp = a;
-			a = b;
-			b = tmp;
-		}
 
 	}
 
