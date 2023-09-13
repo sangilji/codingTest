2ed86ee445541666a0275786a5cd588ce69d13b9
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-13 22:06:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14964 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,17 +30,25 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 		arr = new int[n + 1];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			if (arr[i] == m) {
-				count++;
-			}
 
-			count += map.getOrDefault(arr[i] - m, 0);
-			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		int start = 0;
+		int end = 0;
+		int sum = arr[start];
+		long count = 0;
+		while (end < n) {
+
+			if (sum < m) {
+				sum += arr[++end];
+			} else if (sum > m) {
+				sum -= arr[start++];
+			} else {
+				count++;
+				sum += arr[++end];
+				sum -= arr[start++];
+			}
 		}
 		System.out.println(count);
 
