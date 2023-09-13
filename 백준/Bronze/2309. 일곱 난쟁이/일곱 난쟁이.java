ba5c884466f02ea062a592a2b4cd06000f416565
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-13 22:15:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,44 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-	static int[] arr = new int[9];
-	static int[] result = new int[7];
+
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 100_000;
+	static long[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		arr = new int[9];
+		int sum = 0;
 		for (int i = 0; i < 9; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
 		}
 		Arrays.sort(arr);
-		dfs(0, 0, 0);
-	}
-	static boolean b = false;
-
-	private static void dfs(int current, int sum, int depth) {
-		if (b) {
-			return;
-		}
-		if (depth == 7) {
-			if (sum == 100) {
-				b= true;
-				StringBuilder sb = new StringBuilder();
-				for (int i = 0; i < result.length; i++) {
-					sb.append(result[i]).append("\n");
+		int start = 0;
+		int end = 8;
+		sum -= 100;
+		while (start < end) {
+			int result = arr[start] + arr[end];
+			if (result < sum) {
+				start++;
+			} else if (result > sum) {
+				end--;
+			} else {
+				for (int i = 0; i < 9; i++) {
+					if (i == start || i == end) {
+						continue;
+					}
+					sb.append(arr[i]).append("\n");
 				}
-				System.out.println(sb);
+				break;
 			}
-			return;
 		}
-        if (current >= 9) {
-			return;
-		}
-		dfs(current+1, sum, depth);
-		result[depth] = arr[current];
-		dfs(current+1, sum+arr[current], depth+1);
+		System.out.println(sb);
 
 	}
 
