0a3f4ba3217ce9b1335922895f1e8c854d9a507b
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-14 12:57:29 +0900

    [Gold V] Title: 용액, Time: 404 ms, Memory: 28964 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -2,50 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
-
 
 public class Main {
 
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static List<Integer> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 100_000;
+	static long[] dp;
 
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int start;
+		int end;
+		int abs = Integer.MAX_VALUE;
+		int a = 0;
+		int b = 0;
+		for (int i = 0; i < n; i++) {
+			int tmp = arr[i];
+			start = i + 1;
+			end = n - 1;
+			while (start <= end) {
+				int mid = (start + end) / 2;
+				if (Math.abs(tmp + arr[mid]) < abs) {
+					a = tmp;
+					b = arr[mid];
+					abs = Math.abs(a + b);
+				}
 
-        int n = Integer.parseInt(br.readLine());
-        List<Long> num = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            num.add(Long.parseLong(st.nextToken()));
-        }
-        long min = Long.MAX_VALUE;
-        long a = 0;
-        long b = 0;
-        for (int i = 0; i < n; i++) {
-            int left = i + 1;
-            int right = n - 1;
-            long tmp = num.get(i);
+				if (tmp + arr[mid] <= 0) {
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			}
 
-            while (left <= right) {
-                int mid = (left + right) / 2;
-
-                if (num.get(mid) + tmp < 0) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-                if (Math.abs(num.get(mid) + tmp) < min) {
-                    min = Math.abs(num.get(mid) + tmp);
-                    a = tmp;
-                    b = num.get(mid);
-                }
-            }
-        }
-        System.out.println(a + ": " + b);
-
-
-    }
-
+		}
+		System.out.println(Math.min(a, b) + " " + Math.max(a, b));
+	}
 
 }
