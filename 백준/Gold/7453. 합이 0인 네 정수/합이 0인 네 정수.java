3d696028657f4d3ccb28a5d3116e2984829ae898
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-15 14:30:33 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4068 ms, Memory: 170928 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -1,74 +1,97 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
 
+	static int MOD = 1_000_000_007;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[4][n];
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Long, Integer> map = new HashMap<>();
+	static long[] prefix_gcd;
+	static long[] suffix_gcd;
+	static int[] arr;
+	static long[] tree;
+	static long[] lazy_mul;
+	static long[] lazy_sum;
+	static int SIZE = 150_000;
+	static String s;
+	static int result = 0;
+	static int[] dp;
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                arr[j][i] = Integer.parseInt(st.nextToken());
-            }
-        }
-        int[] ab = new int[n * n];
-        int[] cd = new int[n * n];
-        int idx = 0;
-        for (Integer tmp : arr[0]) {
-            for (Integer tmp1 : arr[1]) {
-                ab[idx++] = tmp + tmp1;
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		int[] b = new int[n];
+		int[] c = new int[n];
+		int[] d = new int[n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			a[i] = Integer.parseInt(st.nextToken());
+			b[i] = Integer.parseInt(st.nextToken());
+			c[i] = Integer.parseInt(st.nextToken());
+			d[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] ab = new int[n * n];
+		int[] cd = new int[n * n];
+		int index = 0;
+		for (int i : a) {
+			for (int j : b) {
+				ab[index++] = i + j;
+			}
+		}
+		index = 0;
+		for (int i : c) {
+			for (int j : d) {
+				cd[index++] = i + j;
 
-        idx = 0;
-        for (Integer tmp : arr[2]) {
-            for (Integer tmp1 : arr[3]) {
-                cd[idx++] = tmp + tmp1;
-            }
-        }
+			}
+		}
+		Arrays.sort(ab);
+		Arrays.sort(cd);
+		int start = 0;
+		int last = n*n-1;
+		long result = 0;
+		while (start < n * n && last >= 0) {
+			int tmp = ab[start] + cd[last];
+			if (tmp < 0) {
+				start++;
+			} else if (tmp > 0) {
+				last--;
+			}else{
+				long aCount = 0;
+				long bCount = 0;
+				for (int i = start; i < n * n; i++) {
+					if (ab[i] == ab[start]) {
+						aCount++;
+					}else{
+						break;
+					}
+				}
+				for (int i = last; i >=0 ; i--) {
+					if (cd[i] == cd[last]) {
+						bCount++;
+					}else{
+						break;
+					}
+				}
+				result+= aCount*bCount;
+				start += aCount;
+				last -= bCount;
+			}
+		}
+		System.out.println(result);
+	}
 
-        Arrays.sort(ab);
-        Arrays.sort(cd);
-        long count = 0;
-        int start = 0;
-        int last = cd.length - 1;
-        while (start < n * n && last >= 0) {
-            int a = ab[start];
-            int b = cd[last];
-            if (a + b > 0) {
-                last--;
-            } else if (a + b < 0) {
-                start++;
-            } else {
-                int aCount = 0;
-                for (int i = start; i < ab.length; i++) {
-                    if (a == ab[i]) {
-                        aCount++;
-                    } else {
-                        break;
-                    }
-                }
-                int bCount = 0;
-                for (int i = last; i >= 0; i--) {
-                    if (b == cd[i]) {
-                        bCount++;
-                    } else {
-                        break;
-                    }
-                }
-                count += (long) aCount * bCount;
-                start += aCount;
-                last -= bCount;
-            }
-        }
-
-        System.out.println(count);
-    }
 }
