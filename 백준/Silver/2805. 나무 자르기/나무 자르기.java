277f2ac5c8ee4bfd53ee7caa6459692a96f76763
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-20 23:27:29 +0900

    [Silver II] Title: 나무 자르기, Time: 524 ms, Memory: 119180 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,37 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static List<Long> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 100_000;
+	static long[] dp;
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());
-		long m = Long.parseLong(st.nextToken());
-		st = new StringTokenizer(br.readLine(), " ");
-		long[] trees = new long[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			trees[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		long left = 1;
-		long right = Arrays.stream(trees).max().getAsLong();
-		long mid;
-		while (left < right) {
-			mid = (left + right) / 2;
-			long count = 0;
-			for (int i = 0; i < n; i++) {
-				if (trees[i] > mid) {
-					count += (trees[i] - mid);
-				}
-			}
-			if (count < m) {
-				right = mid;
-			} else{
-				left = mid +1;
+		int start = 1;
+		int end = 2_000_000_000;
+		int result = 0;
+		while (start <= end) {
+			int mid = (start + end) >> 1;
+			if (check(mid)) {
+				result = mid;
+				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
 		}
-		System.out.println(left-1);
+		System.out.println(result);
+
 	}
+
+	private static boolean check(int num) {
+		long result = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > num) {
+				result += (arr[i] - num);
+			}
+		}
+		return result >= m;
+	}
+
 }
