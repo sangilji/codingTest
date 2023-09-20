d8771a3a28f076868e31be272f2cad49af6a07ca
지상일 <88275989+sangilji@users.noreply.github.com>
2023-09-20 23:34:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17404 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,57 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static List<Long> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr;
+	static int[] arr2;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 100_000;
+	static long[] dp;
+	static StringBuilder sb = new StringBuilder();
 
-		int k = s.nextInt();
-		int n = s.nextInt();
-		int[] line = new int[k];
-		long right = 0;
-		for (int i = 0; i < k; i++) {
-			line[i] = s.nextInt();
-			if(right < line[i]) {
-				right = line[i];
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		long start = 1;
+		long end = Integer.MAX_VALUE;
+		long result = 0;
+		while (start <= end) {
+			long mid = (start + end) >> 1;
+			if (check(mid)) {
+				result = mid;
+				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
 		}
-		right++;
+		System.out.println(result);
 
-		long left = 0; // 탐색 길이 최솟값
-		long mid = 0;
-
-		while (left < right) {
-
-			// 범위 내에서 중간 길이를 구한다.
-			mid = (right + left) / 2;
-
-
-			long count = 0;
-
-			// 구해진 중간 길이로 잘라서 총 몇 개가 만들어지는지를 구한다.
-
-			for (int i = 0; i < k; i++) {
-				count += (line[i] / mid);
-			}
-
-			/*
-			 *  [upper bound 형식]
-			 *
-			 *  mid길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
-			 *  자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
-			 *  그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
-			 */
-			if(count < n) {
-				right = mid;
-			}
-			else {
-				left = mid + 1;
-			}
-
-
-		}
-
-		// UpperBound로 얻어진 값(min)에 -1이 최대 길이가 된다.
-		System.out.println(left - 1);
 	}
+
+	private static boolean check(long num) {
+		long result = 0;
+		for (int i = 0; i < n; i++) {
+			result += arr[i] / num;
+		}
+		return result >= m;
+	}
+
 }
