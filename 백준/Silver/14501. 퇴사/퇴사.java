2e54b9dc38b3512dd499cca8e68c8eedf7793908
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 10:05:14 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,45 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] visit;
-    static int[] dx = new int[]{1, -1, 0, 0};
-    static int[] dy = new int[]{0, 0, 1, -1};
-    static int n;
-    static int m;
 
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] consulting = new int[n + 1][2];
-        int[] dp = new int[n + 1];
-        StringTokenizer st;
-        for (int i = 1; i < n + 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                consulting[i][j] = Integer.parseInt(st.nextToken());
-                dp[i] = consulting[i][j];
-                if (consulting[i][0] - 1 + i > n) {
-                    dp[i] = 0;
-                    break;
-                }
-            }
-        }
+	static int MOD = 1000000000;
+	static int n;
+	static int r;
+	static int m;
+
+	static int[][] arr;
+	static int[] arr2;
+	static List<Integer>[] graph;
+	static List<Integer>[] list;
+	static int[] visit;
+	static int[] in;
+	static int[] out;
+	static int[] tree;
+
+	static int count = 0;
+	static long[][] dp;
+	static long max = Long.MIN_VALUE;
+	static long min = Long.MAX_VALUE;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+
+		arr = new int[n][2];
 
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < i; j++) {
-                if (consulting[j][0] + j <= i) {
-                    dp[i] = Math.max(consulting[i][1] + dp[j], dp[i]);
-                }
-            }
-        }
-        System.out.println(Arrays.stream(dp).max().getAsInt());
-    }
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		recur(0, 0);
+		System.out.println(max);
+	}
+
+	private static void recur(int depth, int sum) {
+
+		if (depth > n) {
+			return;
+		}
+		if (depth == n) {
+			max = Math.max(sum, max);
+			return;
+		}
+
+		recur(depth + arr[depth][0], sum + arr[depth][1]);
+		recur(depth + 1, sum);
+
+	}
 
 }
-
