0d8c46c9fa0f46a42ba8c5d37d5d8fd166b12a84
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 17:25:57 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,53 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int MOD = 1000000000;
 	static int n;
-	static int r;
+	static int k;
 	static int m;
 
 	static int[] arr;
-	static List<Integer>[] graph;
-	static List<Integer>[] list;
-	static int[] visit;
-	static int[] in;
-	static int[] out;
-	static int[] tree;
+	static int[] sum;
+	static int[] num;
+	static int[] arr2;
 
-	static int count = 0;
-	static long[][] dp;
+	static long[] tree_min;
+	static long[] tree_max;
+	static long[] lazy;
+
+	static int min = Integer.MAX_VALUE;
+	static int[][] dp;
+
 	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		arr = new int[m];
-
-		recur(0, 1);
+		recur(1, 0);
 		System.out.println(sb);
 	}
 
-	private static void recur(int depth,int current) {
-		if (depth == m) {
+	private static void recur(int depth, int count) {
+		if (count == m) {
 			for (int i = 0; i < m; i++) {
 				sb.append(arr[i]).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
-		for (int i = current; i <= n; i++) {
-
-			arr[depth] = i;
-			recur(depth + 1, i + 1);
-
+		if (depth > n) {
+			return;
 		}
+		arr[count] = depth;
+		recur(depth + 1, count + 1);
+		recur(depth + 1, count);
 	}
 
 }
