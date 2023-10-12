3da13c8a1a8208ed28b9e5d2c83e1560dcffdb45
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 09:07:08 +0900

    [Silver III] Title: N과 M (5), Time: 332 ms, Memory: 28192 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,44 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer> arr;
-    static boolean[] visit;
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        visit = new boolean[n];
-        arr = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            arr.add(tmp);
-        }
-        arr.sort(Integer::compareTo);
-        dfs( 0, m,"");
-    }
 
-    private static void dfs(int depth, int m, String s) {
-        if (depth == m) {
-            System.out.println(s);
-            return;
-        }
+	static int MOD = 1000000000;
+	static int n;
+	static int r;
+	static int m;
 
-        for (int i = 0; i < arr.size(); i++) {
-            if (!visit[i]) {
-                visit[i]=true;
-                dfs( depth + 1, m, s+arr.get(i)+" ");
-                visit[i] =false;
-            }
-        }
-    }
+	static int[] arr;
+	static int[] arr2;
+	static List<Integer>[] graph;
+	static List<Integer>[] list;
+	static int[] visit;
+	static int[] in;
+	static int[] out;
+	static int[] tree;
+
+	static int count = 0;
+	static long[][] dp;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		visit = new int[n+1];
+		arr2 = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr2);
+
+		recur(0);
+		System.out.println(sb);
+	}
+
+	private static void recur(int depth) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] == 1) {
+				continue;
+			}
+			visit[i] = 1;
+			arr[depth] = arr2[i];
+			recur(depth + 1);
+			visit[i] = 0;
+
+		}
+	}
 
 }
-
