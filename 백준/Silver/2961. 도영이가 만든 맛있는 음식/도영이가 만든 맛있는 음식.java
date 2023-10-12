0e337b07ac2f845ebcbbff71619c12a86654ecae
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 10:01:54 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,43 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[][] taste;
-    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        taste = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            taste[i][0] = Integer.parseInt(st.nextToken());
-            taste[i][1] = Integer.parseInt(st.nextToken());
-        }
+	static int MOD = 1000000000;
+	static int n;
+	static int r;
+	static int m;
+
+	static int[][] arr;
+	static int[] arr2;
+	static List<Integer>[] graph;
+	static List<Integer>[] list;
+	static int[] visit;
+	static int[] in;
+	static int[] out;
+	static int[] tree;
+
+	static int count = 0;
+	static long[][] dp;
+	static long max = Long.MIN_VALUE;
+	static long min = Long.MAX_VALUE;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+
+		arr = new int[n][2];
 
 
-        dfs(0, 0, 1);
-        System.out.println(min);
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		recur(0, 1, 0);
+		System.out.println(min);
+	}
 
-    }
+	private static void recur(int depth,int mul, int sum) {
 
-    private static void dfs(int index, int sum, int mul) {
+		if (depth == n) {
+			if (mul == 1 && sum == 0) {
+				return;
+			}
+			min = Math.min(min,Math.abs(mul-sum));
+			return;
+		}
+		recur(depth + 1, mul * arr[depth][0], sum + arr[depth][1]);
+		recur(depth + 1, mul, sum);
 
-        if (index == n) {
-            if (sum != 0) {
-                min = Math.min(min, Math.abs(sum - mul));
-            }
-            return;
-        }
-
-        dfs(index + 1, sum, mul);
-        dfs(index + 1, sum + taste[index][1], mul * taste[index][0]);
-
-    }
+	}
 
 }
-
