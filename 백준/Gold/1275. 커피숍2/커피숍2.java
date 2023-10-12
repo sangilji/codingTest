c1a04e8dd2a34f1ba48418bf0b174079a51b1d3b
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 12:23:03 +0900

    [Gold I] Title: 커피숍2, Time: 872 ms, Memory: 79684 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int MOD = 1000000000;
+	static int n;
+	static int k;
+	static int m;
+
+	static long[] arr;
+
+	static long[] tree;
+	static long[] lazy;
+
+	static int count = 0;
+	static long[][] dp;
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new long[n + 1];
+		tree = new long[(n + 1) << 2];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+		}
+		init(1, 1, n);
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			long d = Long.parseLong(st.nextToken());
+			if (a > b) {
+				int tmp = a;
+				a = b;
+				b = tmp;
+			}
+			sb.append(get(1, 1, n, a, b)).append("\n");
+			update(1, 1, n, c, d);
+		}
+		System.out.println(sb);
+
+	}
+
+	private static long get(int node, int start, int end, int left, int right) {
+
+		if (right < start || end < left) {
+			return 0;
+		}
+		if (left <= start && end <= right) {
+			return tree[node];
+		}
+		int mid = (start + end) >> 1;
+		return get(node << 1, start, mid, left, right) + get(node << 1 | 1, mid + 1, end, left, right);
+	}
+
+	private static void update(int node, int start, int end, int index, long value) {
+		if (index < start || end < index) {
+			return;
+		}
+		if (start == end) {
+			tree[node] = value;
+			return;
+		}
+		int mid = (start + end) >> 1;
+		update(node << 1, start, mid, index, value);
+		update(node << 1 | 1, mid + 1, end, index, value);
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+
+	private static void prop(int node, int start, int end) {
+		if (lazy[node] != 0) {
+			tree[node] += (end - start + 1) * lazy[node];
+			if (start != end) {
+				lazy[node << 1] += lazy[node];
+				lazy[node << 1 | 1] += lazy[node];
+			}
+			lazy[node] = 0;
+		}
+	}
+
+	private static void init(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = arr[start];
+			return;
+		}
+		int mid = (start + end) >> 1;
+		init(node << 1, start, mid);
+		init(node << 1 | 1, mid + 1, end);
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+/*
+5 2 3
+1
+2
+3
+4
+5
+1 1 5 1
+2 1 5
+2 2 2
+1 1 5 -1
+2 1 1
+ */
+}
