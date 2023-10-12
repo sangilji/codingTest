d3fb0a9441ebd34357f22f08321c287d8460055c
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-12 09:57:35 +0900

    [Silver II] Title: 차이를 최대로, Time: 152 ms, Memory: 15124 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int MOD = 1000000000;
+	static int n;
+	static int r;
+	static int m;
+
+	static int[] arr;
+	static int[] arr2;
+	static List<Integer>[] graph;
+	static List<Integer>[] list;
+	static int[] visit;
+	static int[] in;
+	static int[] out;
+	static int[] tree;
+
+	static int count = 0;
+	static long[][] dp;
+	static long max = Long.MIN_VALUE;
+	static long min = Long.MAX_VALUE;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+
+		arr = new int[n];
+		arr2 = new int[n];
+		visit = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		recur(0);
+		System.out.println(max);
+	}
+
+	private static void recur(int depth) {
+
+		if (depth == n) {
+			int sum = 0;
+			for (int i = 1; i < n; i++) {
+				sum += Math.abs(arr2[i - 1] - arr2[i]);
+			}
+			max = Math.max(sum,max);
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (visit[i] == 1) {
+				continue;
+			}
+			visit[i]=1;
+			arr2[depth] = arr[i];
+			recur(depth + 1);
+			visit[i]=0;
+		}
+	}
+
+}
