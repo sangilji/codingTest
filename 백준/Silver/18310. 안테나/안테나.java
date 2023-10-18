335a28e3f6c23bde341855ebbd69f9fe9f25139e
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-18 14:29:30 +0900

    [Silver III] Title: 안테나, Time: 588 ms, Memory: 40832 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[] arr;
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		if (n % 2 == 0) {
+			System.out.println(arr[n / 2 - 1]);
+			return;
+		} else {
+			System.out.println(arr[n / 2]);
+		}
+
+	}
+
+}
