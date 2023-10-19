cb0f3daf7cf829edc17d2bffa0455017184313a1
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-19 10:31:40 +0900

    [Gold III] Title: 부분 문자열 뽑기 게임, Time: 332 ms, Memory: 18576 KB -BaekjoonHub

--- 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
+++ 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.java
@@ -1,80 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
-
 	static int n;
-	static String result;
-	static int count;
-	static int[] visit = new int[4];
-	static int[] dp = new int[1000001];
-	static int[] ball = new int[3];
+	static int m;
+	static int[][] arr;
+	static int[] dp;
+	static int min = Integer.MAX_VALUE;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(br.readLine());
-		Arrays.fill(dp,-1);
-		if(dfs(n)){
+		// m = Integer.parseInt(st.nextToken());
+		dp = new int[n+1];
+		Arrays.fill(dp, -1);
+		if (recur(n)) {
 			System.out.println(min);
 		}else{
 			System.out.println(-1);
 		}
 
 	}
-	static int min = 10000000;
 
-	private static boolean dfs(int current) {
-		if (current <= 0) {
+	private static boolean recur(int cur) {
+		if (cur <= 0) {
 			return true;
 		}
-		if (current < 10) {
+		if (cur < 10) {
 			return false;
 		}
-		if (dp[current] != -1) {
-			return dp[current] == 1;
+		if (dp[cur] != -1) {
+			return dp[cur] == 1;
 		}
 		int start = 1;
-		while (start <= current) {
+		while (start <= cur) {
 			start*=10;
 		}
 		int count = 0;
 		while (start>1) {
-			int a = current % start;
+			int a = cur % start;
 			start/=10;
 
 			int tmp = 1;
 			while (tmp<=a) {
-				if (!dfs(current - (a / tmp))) {
+				if (!recur(cur - (a / tmp))) {
 					count++;
-					if (current == n) {
+					if (cur == n) {
 						min = Math.min(a / tmp, min);
 					}
 				}
 				tmp *= 10;
 			}
-
 		}
 		if (count > 0) {
-			dp[current] = 1;
+			dp[cur]=1;
 		}else{
-			dp[current] = 0;
+			dp[cur]= 0;
 		}
 		return count > 0;
-		
 	}
 
-	
 
-	private static void init() {
-
-	}
 
 }
