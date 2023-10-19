8268eb669a090edc4b24e0ae9e6bdf3078704422
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-19 09:13:11 +0900

    [Gold IV] Title: 구슬게임, Time: 164 ms, Memory: 17232 KB -BaekjoonHub

--- 백준/Gold/2600. 구슬게임/구슬게임.java
+++ 백준/Gold/2600. 구슬게임/구슬게임.java
@@ -1,79 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	static int n;
-	static String result;
-	static int count;
-	static int[] visit = new int[4];
-	static int[][] dp = new int[501][501];
-	static int[] ball = new int[3];
+	static int[] arr;
+	static int[][] dp;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[3];
+		for (int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		dp = new int[501][501];
 		for (int i = 0; i < 501; i++) {
 			Arrays.fill(dp[i], -1);
 		}
-		for (int i = 0; i < 3; i++) {
-			ball[i] = Integer.parseInt(st.nextToken());
-		}
-		StringBuilder sb = new StringBuilder();
+
 		for (int i = 0; i < 5; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			if (dfs(a, b)) {
+			int k1 = Integer.parseInt(st.nextToken());
+			int k2 = Integer.parseInt(st.nextToken());
+			if (recur(k1, k2)) {
 				sb.append("A\n");
-			}else{
-				sb.append("B\n");
+				continue;
 			}
+			sb.append("B\n");
 		}
 		System.out.println(sb);
 
 
-
-
 	}
 
-	private static boolean dfs(int a, int b) {
-		if (a < 0 || b < 0) {
+	private static boolean recur(int k1, int k2) {
+		if (k1 < 0 || k2 < 0) {
 			return true;
 		}
-		if (a == 0 && b == 0) {
+		if (k1 == 0 && k2 == 0) {
 			return false;
 		}
-		if (dp[a][b] != -1) {
-			return dp[a][b] != 0;
+		if (dp[k1][k2] != -1) {
+			return dp[k1][k2] == 1;
 		}
 		int count = 0;
 		for (int i = 0; i < 3; i++) {
-			if (!dfs(a - ball[i], b)){
-				count += 1;
+			if (!recur(k1 - arr[i], k2)) {
+				count++;
 			}
-			if (!dfs(a, b -ball[i])){
-				count += 1;
+			if (!recur(k1, k2 - arr[i])) {
+				count++;
 			}
 		}
 		if (count > 0) {
-			dp[a][b] = 1;
-		}else {
-			dp[a][b] = 0;
+			dp[k1][k2]=1;
+		}else{
+			dp[k1][k2]=0;
 		}
 		return count > 0;
-	}
-
-	private static void init() {
-
 	}
 
 }
