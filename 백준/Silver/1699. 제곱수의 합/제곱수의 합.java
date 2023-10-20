a3e6612dfd6f67367e6960928ea03a742800c7ac
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-20 13:58:20 +0900

    [Silver II] Title: 제곱수의 합, Time: 208 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,48 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
 	static int n;
-	static List<Integer> arr = new ArrayList<>();
-
+	static int m;
+	static int[] arr;
+	static long[] tree;
 	static int[] dp;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		n = Integer.parseInt(br.readLine());
-		dp = new int[n + 1];
-		Arrays.fill(dp, -1);
-		System.out.println(recur(0));
+		dp = new int[n << 1];
+		Arrays.fill(dp, 123123123);
+		dp[n] = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = 1; j <= Math.sqrt(n); j++) {
+				dp[i] = Math.min(dp[i], dp[i + j * j] + 1);
+			}
+		}
+		System.out.println(dp[0]);
 
 	}
 
+	static int min = 100000;
 
-	private static int recur(int sum) {
-		if (sum > n) {
-			return (int)1e9;
+	private static int recur(int cur) {
+		if (cur > n) {
+			return 12312312;
 		}
-		if (sum == n) {
+		if (cur == n) {
 			return 0;
 		}
-		if (dp[sum] != -1) {
-			return dp[sum];
+		int tmp = 123123123;
+		for (int i = 1; i <= Math.sqrt(n); i++) {
+			tmp = Math.min(tmp, recur(cur + i * i) + 1);
 		}
-		dp[sum] = (int)1e9;
-		for (int i = 1; i * i <= n; i++) {
-			if (sum + i * i > n) {
-				break;
-			}
-			dp[sum] = Math.min(recur(sum + i * i)+1, dp[sum]);
-		}
-		return dp[sum];
+		return tmp;
 	}
-
-
 
 }
