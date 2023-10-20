c0c2a3f2f8bcf6c5bdfde5d10df6ffb557860da8
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-20 13:47:05 +0900

    [Gold IV] Title: 팰린드롬?, Time: 908 ms, Memory: 231688 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,38 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
+	static int n;
+	static int m;
+	static int[] arr;
+	static long[] tree;
+	static int[][] dp;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
 
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int m = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(check(a, b)).append("\n");
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n + 1];
+		dp = new int[n + 1][n + 1];
+		for (int i = 0; i < n + 1; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(recur(a, b) ? 1 : 0).append("\n");
+		}
+		System.out.println(sb);
 
-    private static int check(int start, int last) {
-        int idx = 0;
-        for (int i = start; i < start + (last - start + 1) / 2; i++) {
-            if (arr[i] != arr[last - idx++]) {
-                return 0;
-            }
-        }
-        return 1;
-    }
+	}
+
+	private static boolean recur(int a, int b) {
+		if (b < a || a == b) {
+			return true;
+		}
+		if (dp[a][b] != -1) {
+			return dp[a][b] == 1;
+		}
+		boolean bool;
+		if (arr[a] == arr[b]) {
+			bool = recur(a + 1, b - 1);
+		} else {
+			dp[a][b] = 0;
+			return false;
+		}
+		if (bool) {
+			dp[a][b] = 1;
+		} else {
+			dp[a][b] = 0;
+		}
+		return bool;
+	}
+
 }
