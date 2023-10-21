c46d5656801372e335ac7811cf632db8bfce9b16
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-21 22:02:43 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 532 ms, Memory: 96156 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
-
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int m;
     static int[] arr;
+    static long[] tree;
     static int[] dp;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,29 +22,29 @@ public class Main {
         for (int i = 1; i < n + 1; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int size = 1;
+        int size = 0;
         for (int i = 1; i < arr.length; i++) {
             int tmp = search(size, arr[i]);
             dp[tmp] = arr[i];
             size = Math.max(size, tmp + 1);
         }
-        System.out.println(size-1);
+        System.out.println(size);
     }
 
     private static int search(int size, int target) {
         int left = 0;
         int right = size;
-        while (left < right) {
+        int result = right;
+        while (left <= right) {
             int mid = (left + right) / 2;
 
             if (dp[mid] < target) {
                 left = mid + 1;
             } else {
-                right = mid;
+                result = mid;
+                right = mid - 1;
             }
         }
-        return left;
+        return result;
     }
-
-
 }
