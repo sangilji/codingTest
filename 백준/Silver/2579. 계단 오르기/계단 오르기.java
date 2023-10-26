d96b02b03082329ca54cbba6d607817b92c57f0c
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-26 21:01:31 +0900

    [Silver III] Title: 계단 오르기, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,22 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    static int sum=0;
-    static int[] stair;
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int a =s.nextInt();
-        stair = new int[300];
-        int[] score = new int[300];
-        for(int i =0; i<a;i++){
-            stair[i]=s.nextInt();
+
+    static int MOD = 1000000000;
+    static int n;
+    static int r;
+    static int m;
+
+    static int[] arr;
+
+
+    static int count = 0;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        score[0]=stair[0];
-        score[1]=stair[0]+stair[1];
-        score[2]=Math.max(stair[0],stair[1])+stair[2];
-        for(int i =3 ; i<a;i++){
-            score[i] = Math.max(score[i-2],stair[i-1]+score[i-3])+stair[i];
+        dp = new int[n + 1][4];
+        for (int i = 0; i < n + 1; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        System.out.println(score[a-1]);
+        System.out.println(recur(0, 0));
     }
+
+    private static int recur(int depth, int count) {
+        if (count == 3 || depth > n) {
+            return -123123123;
+        }
+        if (depth == n) {
+            return arr[depth];
+        }
+        if (dp[depth][count] != -1) {
+            return dp[depth][count];
+        }
+        dp[depth][count] = Math.max(recur(depth + 1, count + 1), recur(depth + 2, 1)) + arr[depth];
+        return dp[depth][count];
+    }
+
+
 }
