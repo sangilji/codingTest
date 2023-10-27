679a66bf849c51477c940b17855867d673e8d5a4
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-27 10:30:29 +0900

    [Gold IV] Title: LCS 2, Time: 256 ms, Memory: 48872 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,71 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] a = br.readLine().split("");
-        String[] b = br.readLine().split("");
-        int[] dp;
-        String[] sdp;
-        if (a.length < b.length) {
-            dp = new int[b.length];
-            sdp = new String[b.length];
-            for (int i = 0; i < a.length; i++) {
-                int tmp = 0;
-                String stmp = "";
-                for (int j = 0; j < b.length; j++) {
-                    if (tmp < dp[j]) {
-                        tmp = dp[j];
-                        stmp = sdp[j];
-                        if (a[i].equals(b[j])){
-                            continue;
-                        }
-                    }
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static int[][] dp;
 
-                    if (a[i].equals(b[j])) {
-                        dp[j] = tmp + 1;
-                        sdp[j] = stmp + b[j];
-                    }
+	static int[] arr;
+	static int[] tree;
+	static List<Integer>[] graph;
 
-                }
-            }
-        } else {
-            dp = new int[a.length];
-            sdp = new String[a.length];
-            for (int i = 0; i < b.length; i++) {
-                int tmp = 0;
-                String stmp = "";
-                for (int j = 0; j < a.length; j++) {
-                    if (tmp < dp[j]) {
-                        tmp = dp[j];
-                        stmp = sdp[j];
-                        if (b[i].equals(a[j])){
-                            continue;
-                        }
-                    }
+	static long min = Integer.MAX_VALUE;
 
-                    if (b[i].equals(a[j])) {
-                        dp[j] = tmp + 1;
-                        sdp[j] = stmp + a[j];
-                    }
-                }
-            }
-        }
-        int idx = 0;
-        int max = 0;
-        for (int i = 0; i < dp.length; i++) {
-            if (max < dp[i]) {
-                idx = i;
-                max = dp[i];
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String a = " " + br.readLine();
+		String b = " " + br.readLine();
+		n = a.length();
+		m = b.length();
+		dp = new int[n][m];
+		int[][][] parent = new int[n][m][2];
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				if (a.charAt(i) == b.charAt(j)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+					parent[i][j][0] = i - 1;
+					parent[i][j][1] = j - 1;
+				} else {
+					if (dp[i - 1][j] > dp[i][j - 1]) {
+						dp[i][j] = dp[i - 1][j];
+						parent[i][j][0] = i - 1;
+						parent[i][j][1] = j;
+					} else {
+						dp[i][j] = dp[i][j - 1];
+						parent[i][j][0] = i;
+						parent[i][j][1] = j - 1;
+					}
+				}
+			}
+		}
+		int tmp = dp[n-1][m-1];
+		sb.append(tmp).append("\n");
+		Stack<Character> s = new Stack<>();
+		int x = n - 1;
+		int y = m - 1;
+		while (tmp != 0) {
+			if (dp[x][y] == tmp && a.charAt(x)== b.charAt(y)) {
+				tmp--;
+				s.push(a.charAt(x));
+			}
+			int tx = parent[x][y][0];
+			int ty = parent[x][y][1];
+			x= tx;
+			y =ty;
+		}
+		while (!s.isEmpty()) {
+			sb.append(s.pop());
+		}
+		System.out.println(sb);
 
-        System.out.println(dp[idx]);
-        if (dp[idx] != 0) {
-            System.out.println(sdp[idx]);
-        }
-    }
+	}
+
 }
