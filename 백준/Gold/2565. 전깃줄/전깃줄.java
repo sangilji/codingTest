fa00f94db48f5acb0048385eb253f9e16b7f91af
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-27 09:10:28 +0900

    [Gold V] Title: 전깃줄, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,35 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
-
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static int[] dp;
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static int[] dp;
+	static int[][] arr;
+	static int[] tree;
+	static List<Integer>[] graph;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1][2];
-        dp = new int[501];
+	static long min = Integer.MAX_VALUE;
 
-        for (int i = 1; i < n + 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        Arrays.sort(arr,Comparator.comparingInt(o->o[0]));
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < arr[i][1]; j++) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n + 1][2];
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
 
-                dp[arr[i][1]] = Math.max(dp[arr[i][1]], dp[j] + 1);
-            }
-        }
-        System.out.println(n-Arrays.stream(dp).max().getAsInt());
-
-    }
+		dp = new int[n + 1];
+		int max = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i][1] > arr[j][1]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+					max = Math.max(max, dp[i]);
+				}
+			}
+		}
+		System.out.println(n-max);
+	}
 
 }
