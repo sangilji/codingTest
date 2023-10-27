0132568f5eb7f80759793fd23d6242c1a3d4d100
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-27 10:37:12 +0900

    [Gold IV] Title: 팰린드롬?, Time: 880 ms, Memory: 230708 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,27 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int MOD = 1_000_000_009;
 	static int n;
 	static int m;
-	static int[] arr;
-	static long[] tree;
 	static int[][] dp;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+
+	static int[] arr;
+	static int[] tree;
+	static List<Integer>[] graph;
+
+	static long min = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		n = Integer.parseInt(br.readLine());
 		arr = new int[n + 1];
 		dp = new int[n + 1][n + 1];
 		for (int i = 0; i < n + 1; i++) {
 			Arrays.fill(dp[i], -1);
 		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
@@ -31,32 +36,24 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			sb.append(recur(a, b) ? 1 : 0).append("\n");
+			sb.append(recur(a, b)).append("\n");
 		}
 		System.out.println(sb);
 
 	}
 
-	private static boolean recur(int a, int b) {
-		if (b < a || a == b) {
-			return true;
+	private static int recur(int a, int b) {
+		if (a > b) {
+			return 1;
+		}
+		if (arr[a] != arr[b]) {
+			return 0;
 		}
 		if (dp[a][b] != -1) {
-			return dp[a][b] == 1;
+			return dp[a][b];
 		}
-		boolean bool;
-		if (arr[a] == arr[b]) {
-			bool = recur(a + 1, b - 1);
-		} else {
-			dp[a][b] = 0;
-			return false;
-		}
-		if (bool) {
-			dp[a][b] = 1;
-		} else {
-			dp[a][b] = 0;
-		}
-		return bool;
+
+		return dp[a][b] = recur(a + 1, b - 1);
 	}
 
 }
