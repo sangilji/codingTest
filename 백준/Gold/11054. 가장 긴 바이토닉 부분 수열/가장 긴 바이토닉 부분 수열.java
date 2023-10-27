ff941e489729b7813e058d68b43daa0d10ce5aba
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-27 09:18:20 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 160 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,34 +1,53 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[] a = new int[n];
-        int[] dp = new int[n];
-        int[] dpR = new int[n];
-        for(int i =0;i<n;i++){
-            a[i]=s.nextInt();
-        }
-        int result=0;
-        for(int i =0;i<n;i++){
-            dp[i]=1;
-            for(int j =0;j<i;j++){
-                if(a[i]>a[j] &&dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
-                }
-            }
-        }
-        for(int i=n-1;i>=0;i--){
-            dpR[i]=1;
-            for(int j=n-1;j>=i;j--){
-                if(a[i]>a[j]&& dpR[i]<dpR[j]+1){
-                    dpR[i]=dpR[j]+1;
-                }
-            }
-            result = Math.max(result,dpR[i]+dp[i]);
-        }
-        System.out.println(result-1);
-    }
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static int[] dp_left;
+	static int[] dp_right;
+	static int[] arr;
+	static int[] tree;
+	static List<Integer>[] graph;
+
+	static long min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[n + 2];
+		dp_left = new int[n + 2];
+		dp_right = new int[n + 2];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp_left[i] = Math.max(dp_left[i], dp_left[j] + 1);
+				}
+			}
+		}
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = n + 1; j > i; j--) {
+				if (arr[i] > arr[j]) {
+					dp_right[i] = Math.max(dp_right[i], dp_right[j] + 1);
+				}
+			}
+		}
+		int max = 0;
+		for (int i = 1; i <= n ; i++) {
+			max = Math.max(max, dp_right[i] + dp_left[i]-1);
+		}
+		System.out.println(max);
+	}
+
 }
