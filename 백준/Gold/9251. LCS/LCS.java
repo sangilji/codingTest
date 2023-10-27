5d6bcecd38dbf9381c688d285103c9526bd9125f
지상일 <88275989+sangilji@users.noreply.github.com>
2023-10-27 09:23:56 +0900

    [Gold V] Title: LCS, Time: 172 ms, Memory: 19644 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,22 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String a = " " + s.next();
-        String b = " " + s.next();
-        int[][] dp = new int[a.length()][b.length()];
-        for(int i =1;i<dp.length;i++){
-            for(int j =1;j<dp[i].length;j++){
+	static int MOD = 1_000_000_009;
+	static int n;
+	static int m;
+	static int[][] dp;
 
-                if(a.charAt(i)==b.charAt(j)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
+	static int[] arr;
+	static int[] tree;
+	static List<Integer>[] graph;
 
-            }
-        }
-        System.out.println(dp[dp.length-1][dp[0].length-1]);
-    }
+	static long min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String a = " " + br.readLine();
+		String b = " " + br.readLine();
+		n = a.length();
+		m = b.length();
+		dp = new int[n][m];
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				if (a.charAt(i) == b.charAt(j)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				}else{
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+				}
+			}
+		}
+		System.out.println(dp[n-1][m-1]);
+
+	}
+
 }
