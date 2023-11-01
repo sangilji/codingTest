e1cf88b77187c1a7be57f0c98497202a1e676ebb
지상일 <88275989+sangilji@users.noreply.github.com>
2023-11-01 22:42:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 700 ms, Memory: 141392 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,27 +1,49 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] graph;
-    static boolean[] visit;
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int m = s.nextInt();
-        graph = new int[n + 1][n + 1];
-        visit = new boolean[n + 1];
-        for (int i = 0; i < m; i++) {
-            int a = s.nextInt();
-            int b = s.nextInt();
-            graph[a][b] = graph[b][a] = 1;
+    static int MOD = 1000000000;
+    static int n;
+    static int r;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static List<Integer>[] list;
+
+    static int count = 0;
+    static int[] visit;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visit = new int[n + 1];
+        list = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
         }
-        int count = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
         for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                bfs(i);
+            if (visit[i] == 0) {
+                dfs(i);
                 count++;
             }
         }
@@ -29,17 +51,15 @@ public class Main {
 
     }
 
-    private static void bfs(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        while (!q.isEmpty()) {
-            int tmp = q.poll();
-            for (int i = 1; i < graph.length; i++) {
-                if (graph[tmp][i] == 1 && !visit[i]) {
-                    visit[i] = true;
-                    q.add(i);
-                }
+    private static void dfs(int cur) {
+        visit[cur] = 1;
+        for (int i = 0; i < list[cur].size(); i++) {
+            if (visit[list[cur].get(i)] == 1) {
+                continue;
             }
+            dfs(list[cur].get(i));
         }
     }
+
+
 }
