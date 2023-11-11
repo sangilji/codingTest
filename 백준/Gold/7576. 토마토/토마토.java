93947b6159e5ff38c08b1827f8d28e239d204700
지상일 <88275989+sangilji@users.noreply.github.com>
2023-11-11 19:05:56 +0900

    [Gold V] Title: 토마토, Time: 664 ms, Memory: 110600 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,65 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] box;
-    static Queue<int[]> tomatoes;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int M;
-    static int N;
+
+    static int MOD = 1_000_000_000;
+    static int n;
+    static int r;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<Integer>[] list;
+
+    static int count = 0;
+    static int[] arr2;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        box = new int[N][M];
-        tomatoes = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
-                if (box[i][j] == 1) {
-                    tomatoes.add(new int[]{i, j, 0});
-                }
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         System.out.println(bfs());
+
+
     }
 
     private static int bfs() {
-        int max = 0;
-        while (!tomatoes.isEmpty()) {
-            int[] tmp = tomatoes.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-            max = Math.max(max, tmp[2]);
-            for (int i = 0; i < 4; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= N || ty >= M) {
-                    continue;
-                }
-                if (box[tx][ty] == 0) {
-                    tomatoes.add(new int[]{tx, ty, tmp[2] + 1});
-                    box[tx][ty] = 1;
+        Queue<int[]> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1) {
+                    q.add(new int[]{i, j, 0});
                 }
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (box[i][j] == 0) {
+        int max = 0;
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            max = Math.max(max, tmp[2]);
+            for (int i = 0; i < 4; i++) {
+                int tx = tmp[0] + dx[i];
+                int ty = tmp[1] + dy[i];
+                if (tx < 0 || ty < 0 || tx >= n || ty >= m) {
+                    continue;
+                }
+                if (arr[tx][ty] == 0) {
+                    q.add(new int[]{tx, ty, tmp[2] + 1});
+                    arr[tx][ty] = tmp[2] + 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
                     return -1;
                 }
             }
         }
         return max;
     }
+
 
 }
