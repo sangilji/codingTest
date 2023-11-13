c2e131b7a43854355080d7b6b2d704923b818b62
지상일 <88275989+sangilji@users.noreply.github.com>
2023-11-13 09:32:44 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 740 ms, Memory: 69600 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,49 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final int INF = 200_000_000;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int[][] costs = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i != j) {
-                    costs[i][j] = INF;
-                }
-            }
-        }
-        for (int i = 0; i < e; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            costs[a][b] = costs[b][a] = c;
-        }
-        st = new StringTokenizer(br.readLine(), " ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+	static int MOD = 1_000_000_009;
+	static int INF = 1_500_000_000;
+	static int n;
+	static int m;
+	static int k;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    costs[j][k] = Math.min(costs[j][k], costs[j][i] + costs[i][k]);
-                }
-            }
+	static int[][] arr;
+	static int[][] visit;
+	static List<Integer> list = new ArrayList<>();
+	static List<int[]>[] graph;
+	static int[] alpha;
+	static int[] dx = {0, 0, 0, 1, -1};
+	static int[] dy = {0, 1, -1, 0, 0};
 
-        }
-        if ((costs[1][a] ==INF || costs[a][b]==INF || costs[b][n] ==INF) ||(costs[1][b] ==INF || costs[b][a]==INF || costs[a][n] ==INF)){
-            System.out.println(-1);
-            return;
-        }
-        System.out.println(Math.min(costs[1][a] + costs[a][b] + costs[b][n], costs[1][b] + costs[b][a] + costs[a][n]));
+	static int count = 0;
 
-    }
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		graph = new List[n + 1];
+		for (int i = 1; i <= n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graph[a].add(new int[] {b, c});
+			graph[b].add(new int[] {a, c});
+		}
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int[] tmp = bfs(a,b);
+		int[] tmp2 = bfs(b, a);
+		if ((tmp[0] == INF || tmp[1] == INF || tmp2[2] == INF) || (tmp2[0] == INF || tmp2[1] == INF || tmp[2] == INF)) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(Math.min(tmp[0] + tmp[1] + tmp2[2], tmp2[0] + tmp2[1] + tmp[2]));
+	}
+
+	private static int[] bfs(int cur, int b) {
+
+		PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+		int[] dist = new int[n + 1];
+		Arrays.fill(dist, INF);
+		dist[cur] = 0;
+		q.add(new int[] {cur, 0});
+
+		while (!q.isEmpty()) {
+			int[] tmp = q.poll();
+
+			for (int i = 0; i < graph[tmp[0]].size(); i++) {
+				int[] next = graph[tmp[0]].get(i);
+
+				if (dist[next[0]] > tmp[1] + next[1]) {
+					dist[next[0]] = tmp[1] + next[1];
+					q.add(new int[] {next[0], dist[next[0]]});
+				}
+			}
+		}
+		return new int[] {dist[1], dist[b], dist[n]};
+	}
 
 }
