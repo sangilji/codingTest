c62861f7299f54857be36b66ab681c4f06fc9c7a
지상일 <88275989+sangilji@users.noreply.github.com>
2023-11-21 10:34:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.59 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         Map<String,Integer> map = new HashMap<>();
         map.put("R",0);map.put("T",0);map.put("C",0);map.put("F",0);
         map.put("J",0);map.put("M",0);map.put("A",0);map.put("N",0);
@@ -14,26 +14,26 @@ class Solution {
             }
         }
         if(map.get("R")>=map.get("T")){
-            answer+="R";
+            answer.append("R");
         } else {
-            answer+="T";
+            answer.append("T");
         }
         if(map.get("C")>=map.get("F")){
-            answer+="C";
+            answer.append("C");
         } else {
-            answer+="F";
+            answer.append("F");
         }
         if(map.get("J")>=map.get("M")){
-            answer+="J";
+            answer.append("J");
         } else {
-            answer+="M";
+            answer.append("M");
         }
         if(map.get("A")>=map.get("N")){
-            answer+="A";
+            answer.append("A");
         } else {
-            answer+="N";
+            answer.append("N");
         }
         
-        return answer;
+        return answer.toString();
     }
 }
