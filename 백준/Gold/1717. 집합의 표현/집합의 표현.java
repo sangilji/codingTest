531054e826bf06cbf5d832a1ae1d7e14eda43e1b
지상일 <88275989+sangilji@users.noreply.github.com>
2023-12-08 20:53:09 +0900

    [Gold V] Title: 집합의 표현, Time: 468 ms, Memory: 56240 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,48 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int[] parent;
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<int[]>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		parent = new int[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			parent[i] = i;
-		}
-		int m = s.nextInt();
 
-		for (int i = 0; i < m; i++) {
-			int command = s.nextInt();
-			if (command == 0) {
-				int a = s.nextInt();
-				int b = s.nextInt();
-				a = find(a);
-				b = find(b);
-				if (a < b) {
-					parent[b] = a;
-				} else {
-					parent[a] = b;
-				}
-			} else {
-				int a = s.nextInt();
-				int b = s.nextInt();
-				a = find(a);
-				b = find(b);
-				if (a == b) {
-					System.out.println("YES");
-				} else {
-					System.out.println("NO");
-				}
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int findA = find(b);
+            int findB = find(c);
+            if (a == 0) {
+                if (findA != findB) {
+                    if (findA < findB) {
+                        parent[findB] = findA;
+                    } else {
+                        parent[findA] = findB;
+                    }
+                }
+            } else {
+                if (findA == findB) {
+                    sb.append("YES\n");
+                } else {
+                    sb.append("NO\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 
-		}
-	}
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
 
-	private static int find(int a) {
-		if (a == parent[a]) {
-			return a;
-		}
-		return parent[a] = find(parent[a]);
-	}
+
 }
