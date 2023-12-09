4561a56a675e70ca98c1a6728821d9bd34a557b4
지상일 <88275989+sangilji@users.noreply.github.com>
2023-12-09 19:54:55 +0900

    [Gold IV] Title: 친구비, Time: 228 ms, Memory: 19808 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,72 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int k;
-	static int[][] dp;
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<int[]>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	static int[] arr;
-	static List<Integer>[] list;
-	static int[] visit;
 
-	static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		arr = new int[n + 1];
-		visit = new int[n + 1];
-		list = new List[n + 1];
-		for (int i = 1; i <=n; i++) {
-			list[i] = new ArrayList<>();
-		}
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			list[b].add(a);
-		}
+        parent = new int[n + 1];
+        sz = new int[n + 1];
 
-		for (int i = 1; i <=n; i++) {
-			if (visit[i] == 0) {
-				count+=dfs(i);
-			}
-		}
-		if (count <= k) {
-			System.out.println(count);
-		}else{
-			System.out.println("Oh no");
-		}
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            sz[i] = Integer.parseInt(st.nextToken());
+        }
 
-	}
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int findA = find(a);
+            int findB = find(b);
+            if (findA != findB) {
+                if (findA < findB) {
+                    sz[findA] = Math.min(sz[findA], sz[findB]);
+                    sz[findB] = 0;
+                    parent[findB] = findA;
+                } else {
+                    sz[findB] = Math.min(sz[findA], sz[findB]);
+                    sz[findA] = 0;
+                    parent[findA] = findB;
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += sz[i];
+        }
+        if (sum <= k) {
+            System.out.println(sum);
+        } else {
+            System.out.println("Oh no");
+        }
 
-	private static int dfs(int cur) {
-		visit[cur] = 1;
-		int tmp = arr[cur];
-		for (int i = 0; i < list[cur].size(); i++) {
-			if (visit[list[cur].get(i)] == 1) {
-				continue;
-			}
-			tmp = Math.min(tmp, dfs(list[cur].get(i)));
-		}
-		return tmp;
-	}
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
 
 }
