78d304af61333bd0114f08568745146ed892a1e5
지상일 <88275989+sangilji@users.noreply.github.com>
2023-12-09 23:10:34 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 684 ms, Memory: 50876 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,47 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static int[] parent;
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<int[]>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int v = s.nextInt();
-		int e = s.nextInt();
 
-		parent = new int[v + 1];
-		for (int i = 1; i <= v; i++) {
-			parent[i] = i;
-		}
-		int[][] nodes = new int[e][3];
-		for (int i = 0; i < nodes.length; i++) {
-			nodes[i][0] = s.nextInt();
-			nodes[i][1] = s.nextInt();
-			nodes[i][2] = s.nextInt();
-		}
-		Arrays.sort(nodes, Comparator.comparingInt(o -> o[2]));
-		int result = 0;
-		for (int i = 0; i < nodes.length; i++) {
-			int a = check(nodes[i][0]);
-			int b = check(nodes[i][1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m][3];
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[2]));
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            int a = find(arr[i][0]);
+            int b = find(arr[i][1]);
 
-			if (a != b) {
-				result += nodes[i][2];
-				if (a < b) {
-					parent[b]=a;
-				} else {
-					parent[a]=b;
-				}
-			}
-		}
-		System.out.println(result);
-	}
+            if (a != b) {
+                count += arr[i][2];
+                if (a < b) {
+                    parent[b] = a;
+                } else {
+                    parent[a] = b;
 
-	private static int check(int i) {
-		if (i == parent[i]) {
-			return i;
-		}
-		return check(parent[i]);
-	}
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+
 }
