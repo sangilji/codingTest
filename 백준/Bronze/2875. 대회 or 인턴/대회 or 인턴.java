2703823a49f68ed7f0f70eb783689b90678a5154
지상일 <88275989+sangilji@users.noreply.github.com>
2023-12-29 20:50:46 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        for (int i = 0; i <= k; i++) {
+            int a = n - i;
+            int b = m - (k - i);
+            a /= 2;
+            int tmp = Math.min(a, b);
+            count = Math.max(count, tmp);
+        }
+        System.out.println(count);
+
+    }
+
+
+}
