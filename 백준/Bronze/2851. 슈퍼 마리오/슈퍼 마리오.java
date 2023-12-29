b005c9948f690322d4ea0adf26644cd5aea5b718
지상일 <88275989+sangilji@users.noreply.github.com>
2023-12-29 20:54:43 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,34 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[11];
-		for (int i = 1; i < 11; i++) {
-			arr[i] = arr[i - 1] + Integer.parseInt(br.readLine());
-		}
-		int result =0;
-		for (int i = 1; i < 11; i++) {
-			if (Math.abs(arr[i] - 100) <= Math.abs(result - 100)) {
-				result = arr[i];
-			}
-		}
-		System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int sum = arr[0];
 
-	}
+        for (int i = 1; i < 10; i++) {
+            if (Math.abs(100 - (sum + arr[i])) <= Math.abs(100 - sum)) {
+                sum += arr[i];
+            } else {
+                break;
+            }
+        }
+        System.out.println(sum);
 
-
-
+    }
 
 
 }
