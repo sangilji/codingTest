3d8edbf91c62943ce4e678379f7f526838213352
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-01 19:32:56 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,31 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        boolean[][] pos = new boolean[101][101];
-        int area = 0;
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[101][101];
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    if (!pos[j][k]) {
-                        pos[j][k] = true;
-                        area++;
-                    }
+                    arr[j][k]++;
                 }
             }
         }
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                if (arr[i][j] > 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
 
-        System.out.println(area);
 
     }
+
+
 }
