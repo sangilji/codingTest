5a028fb8f5beb768248ac786a10684b9333456b6
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-02 19:08:25 +0900

    [Silver III] Title: 숫자 야구, Time: 128 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,83 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int[][] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n][3];
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 3; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		int result = 0;
-		for (int i = 123; i <= 999; i++) {
-			if (!check(String.valueOf(i))) {
-				continue;
-			}
-			boolean b = true;
-			for (int j = 0; j < n; j++) {
-				int strike = checkStrike(String.valueOf(i), String.valueOf(arr[j][0]));
-				if (i == 328 || i == 324) {
-				}
-				if (strike != arr[j][1]) {
-					b = false;
-					break;
-				}
-				int ball = checkBall(String.valueOf(i), String.valueOf(arr[j][0]));
-				if (i == 328 || i == 324) {
-				}
-				if (ball != arr[j][2]) {
-					b= false;
-					break;
-				}
-			}
-			if (b) {
-				result++;
-			}
-		}
-		System.out.println(result);
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
 
-	}
+        arr = new int[n][2];
+        int[][] arr2 = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String[] tmp = st.nextToken().split("");
+            for (int j = 0; j < 3; j++) {
+                arr2[i][j] = Integer.parseInt(tmp[j]);
+            }
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (i == j) {
+                    continue;
+                }
+                for (int k = 1; k <= 9; k++) {
+                    if (i == k || j == k) {
+                        continue;
+                    }
+                    boolean b = true;
+                    for (int l = 0; l < n; l++) {
+                        int strike = 0;
+                        int ball = 0;
+                        if (arr2[l][0] == i) {
+                            strike++;
+                        }
+                        if (arr2[l][0] == j || arr2[l][0] == k) {
+                            ball++;
+                        }
+                        if (arr2[l][1] == j) {
+                            strike++;
+                        }
+                        if (arr2[l][1] == i || arr2[l][1] == k) {
+                            ball++;
+                        }
+                        if (arr2[l][2] == k) {
+                            strike++;
+                        }
+                        if (arr2[l][2] == j || arr2[l][2] == i) {
+                            ball++;
+                        }
+                        if (strike != arr[l][0] || ball != arr[l][1]) {
+                            b = false;
+                            break;
+                        }
+                    }
+                    if (b) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
 
-	private static int checkBall(String a, String b) {
-		int count = 0;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (i == j) {
-					continue;
-				}
-				if (a.charAt(i) == b.charAt(j)) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
+    }
 
-	private static int checkStrike(String a, String b) {
-		int count = 0;
-		for (int i = 0; i < 3; i++) {
-			if (a.charAt(i) == b.charAt(i)) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	private static boolean check(String s) {
-		return !s.contains("0") && s.length()==Arrays.stream(s.split("")).distinct().count();
-	}
 
 }
