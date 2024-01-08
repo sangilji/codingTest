a91dab0d5dad7adf370b370016ca9cb4cb6c19aa
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-08 11:39:12 +0900

    [Silver III] Title: 팩토리얼 나누기, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/15996. 팩토리얼 나누기/팩토리얼 나누기.java
+++ 백준/Silver/15996. 팩토리얼 나누기/팩토리얼 나누기.java
@@ -1,54 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	static int MOD = 1_000_000;
-	static int n;
-	static int m;
-	static Map<Integer, Integer>[] list;
-	static Map<Integer, Integer> map = new HashMap<>();
-	static long[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        long tmp = n;
+        long count = 0;
+        while (tmp != 0) {
+            count += tmp / k;
+            tmp /= k;
+        }
+        System.out.println(count);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int count = 0;
-		while (n >= 1) {
-			count += n / m;
-			n /= m;
-		}
-		System.out.println(count);
-
-	}
-
-	private static long gcd(long a, long b) {
-		if (a > b) {
-			long tmp = a;
-			a = b;
-			b = tmp;
-		}
-		if (a == 0) {
-			return b;
-		}
-		while (true) {
-			if (b % a == 0) {
-				return a;
-			}
-			b %= a;
-
-			long tmp = a;
-			a = b;
-			b = tmp;
-		}
-
-	}
 
 }
