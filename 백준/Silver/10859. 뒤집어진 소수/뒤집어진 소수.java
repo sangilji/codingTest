e2af13724a428c4e437385a812cbdb130cc34719
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-08 09:23:29 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 1788 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
@@ -1,55 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 1_000_000_000;
+    static long n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	static long n;
-	static long m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String tmp = br.readLine();
+        n = Long.parseLong(tmp);
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.println("no");
+                return;
+            }
+        }
+        for (int i = tmp.length() - 1; i >= 0; i--) {
+            if (tmp.charAt(i) == '6') {
+                sb.append(9);
+            } else if (tmp.charAt(i) == '9') {
+                sb.append(6);
+            } else if (tmp.charAt(i) == '3' || tmp.charAt(i) == '4' || tmp.charAt(i) == '7') {
+                System.out.println("no");
+                return;
+            } else {
+                sb.append(tmp.charAt(i));
+            }
+        }
+        long num = Long.parseLong(sb.toString());
+        if (num == 1) {
+            System.out.println("no");
+            return;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                System.out.println("no");
+                return;
+            }
+        }
+        System.out.println("yes");
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
-		String tmp = br.readLine();
-		m = Long.parseLong(tmp);
-		for (int i = 2; i <= Math.sqrt(m); i++) {
-			if (m % i == 0) {
-				System.out.println("no");
-				return;
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = tmp.length() - 1; i >= 0; i--) {
-			if (tmp.charAt(i) == '6') {
-				sb.append('9');
-			} else if (tmp.charAt(i) == '9') {
-				sb.append('6');
-			} else if (tmp.charAt(i) == '3' || tmp.charAt(i) == '4' || tmp.charAt(i) == '7') {
-				System.out.println("no");
-				return;
-			} else {
-				sb.append(tmp.charAt(i));
-			}
-		}
-		try {
-			n = Long.parseLong(sb.toString());
-		} catch (Exception e) {
-			System.out.println("no");
-			return;
-		}
-		if (n == 1) {
-			System.out.println("no");
-			return;
-		}
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) {
-				System.out.println("no");
-				return;
-			}
-		}
-
-		System.out.println("yes");
-
-	}
 
 }
