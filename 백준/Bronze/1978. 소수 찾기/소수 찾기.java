0f4bc49bc95f09a259af1aa6bbe0b0eba2742d8a
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-08 11:18:18 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,45 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int INF = 1_000_000_000;
+    static int n;
+    static int m;
+    static List<Integer>[] list;
+    static int[] parent;
+    static int[] score;
+    static int[] cards;
+    static int[][] arr;
+    static int[][][] dp;
+    static int[] sz;
+    static int[] visit;
+    static long count;
 
-	static int MOD = 1_000_000;
-	static long n;
-	static int m;
-	static Map<Integer, Integer>[] list;
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int[] prime = new int[1001];
+        prime[0] = 1;
+        prime[1] = 1;
+        for (int i = 2; i <= 1000; i++) {
+            if (prime[i] == 1) {
+                continue;
+            }
+            for (int j = i * i; j <= 1000; j += i) {
+                prime[j] = 1;
+            }
+        }
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (prime[tmp] == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		arr = new int[10001];
-		init();
-		n = Long.parseLong(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int count = 0;
-		for (int i = 0; i < n; i++) {
-			count += arr[Integer.parseInt(st.nextToken())];
-		}
-		System.out.println(n - count);
-
-	}
-
-	private static void init() {
-		arr[1] = 1;
-		arr[0] = 1;
-		for (int i = 2; i <= Math.sqrt(10000); i++) {
-			if (arr[i] == 1) {
-				continue;
-			}
-			for (int j = i + i; j <= 10000; j += i) {
-				arr[j] = 1;
-			}
-		}
-	}
+    }
 
 }
