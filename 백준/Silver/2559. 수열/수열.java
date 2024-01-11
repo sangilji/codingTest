296c4285de998935c9f2e22141948421775d6c7d
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-11 16:31:00 +0900

    [Silver III] Title: 수열, Time: 272 ms, Memory: 23440 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int INF = 1_000_000_007;
+    static int n;
+    static int r;
+    static int m;
+    static int c;
+    static int x;
+    static int[] arr;
+    static long[] tree;
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<int[]>[] list;
+
+    static long count = 0;
+    static int[] arr2;
+    static int[][] dp;
+    static long[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] += arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        count = Integer.MIN_VALUE;
+        for (int i = m; i <= n; i++) {
+            count = Math.max(count, arr[i] - arr[i - m]);
+        }
+        System.out.println(count);
+    }
+
+}
