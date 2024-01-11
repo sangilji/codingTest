4078225357c2a69f2938c6002db62ab780f4ccf7
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-11 11:19:34 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 636 ms, Memory: 105692 KB -BaekjoonHub

--- 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.java
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.java
@@ -1,60 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_007;
-	static int n;
-	static int m;
-	static List<int[]> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Long, Integer> map = new HashMap<>();
-	static long[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 150_000;
-	static String s;
+    static int INF = 1_000_000_007;
+    static int n;
+    static int r;
+    static int m;
+    static int c;
+    static int x;
+    static int[] arr;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<int[]>[] list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int t = Integer.parseInt(br.readLine());
-		for (int i = 0; i < t; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
-			arr = new long[m + 1];
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= m; j++) {
-				arr[j] = arr[j - 1] + Integer.parseInt(st.nextToken());
-			}
-			long result = 0;
-			for (int j = 1; j <= m; j++) {
-				if (arr[j] < n) {
-					map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
-					continue;
-				}
-				long tmp = arr[j]%n;
-				int count = map.getOrDefault(tmp, 0);
-				result+=count;
-				if (tmp == 0) {
-					result+=1;
-				}
+    static long count = 0;
+    static int[] arr2;
+    static int[][] dp;
+    static long[] visit;
 
-				map.put(tmp, count + 1);
-			}
-			sb.append(result).append("\n");
-			map.clear();
-		}
-		System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new int[m + 1];
+            int[] division = new int[n + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= m; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            long sum = 0;
+            long count = 0;
+            for (int i = 1; i <= m; i++) {
+                sum += arr[i];
+                if (sum % n == 0) {
+                    count++;
+                }
+                count += division[(int) (sum % n)];
+                division[(int) (sum % n)]++;
+            }
+            sb.append(count).append("\n");
+        }
 
-	}
-
+        System.out.println(sb);
+    }
 }
