7e64d692adda444b0ade816e850c40dc5e944059
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-12 14:08:10 +0900

    [Silver II] Title: 연속합, Time: 272 ms, Memory: 22876 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,47 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000;
-	static int n;
-	static int m;
-	static List<int[]> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 1_000_000;
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int r;
+    static int m;
+    static int c;
+    static int x;
+    static int[] arr;
+    static long[] tree;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		long max = -10000;
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n + 1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken()) + arr[i - 1];
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<int[]>[] list;
 
-		}
-		int[] prefix_min = new int[n + 1];
-		prefix_min[0] = 2000;
-		for (int i = 1; i <= n; i++) {
-			prefix_min[i] = Math.min(arr[i-1],prefix_min[i - 1]);
-		}
+    static long count = 0;
+    static int[] arr2;
+    static int[][] dp;
+    static long[] visit;
+    static String s;
 
-		for (int i = 1; i <= n; i++) {
-			max = Math.max(arr[i] - prefix_min[i],max);
-		}
-		System.out.println(max);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n + 1];
+        int[] min = new int[n + 1];
+        min[0] = INF;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+            min[i] = Math.min(min[i - 1], arr[i - 1]);
+        }
+        count = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            count = Math.max(count, arr[i] - min[i]);
+        }
+        System.out.println(count);
+
+    }
 
 }
