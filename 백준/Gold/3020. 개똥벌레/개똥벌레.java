d4be922bcc7227eea7923d28abe58f45263bc621
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-12 11:19:13 +0900

    [Gold V] Title: 개똥벌레, Time: 304 ms, Memory: 28880 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,58 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000;
-	static int n;
-	static int m;
-	static List<int[]> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 150_000;
+    static int INF = 1_000_000_007;
+    static int n;
+    static int r;
+    static int m;
+    static int c;
+    static int x;
+    static int[] arr;
+    static long[] tree;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[m + 2];
-		for (int i = 1; i <= n; i++) {
-			int tmp = Integer.parseInt(br.readLine());
-			if (i % 2 == 0) {
-				arr[m-tmp+1] +=1;
-			}else{
-				arr[1] += 1;
-				arr[1+tmp] -=1;
-			}
-		}
-		for (int i = 1; i <= m; i++) {
-			arr[i] += arr[i - 1];
-		}
-		int min = 10000000;
-		for (int i = 1; i <= m; i++) {
-			min = Math.min(min, arr[i]);
-		}
-		int count = 0;
-		for (int i = 1; i <=m; i++) {
-			if (min == arr[i]) {
-				count++;
-			}
-		}
-		sb.append(min).append(" ").append(count);
-		System.out.println(sb);
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<int[]>[] list;
 
-	}
+    static long count = 0;
+    static int[] arr2;
+    static int[][] dp;
+    static long[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m + 2];
+        for (int i = 1; i <= n; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+            if (i % 2 == 0) {
+                arr[m - tmp + 1] += 1;
+            } else {
+                arr[1] += 1;
+                arr[1 + tmp] -= 1;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            arr[i] += arr[i - 1];
+        }
+        int min = 10000000;
+        for (int i = 1; i <= m; i++) {
+            min = Math.min(min, arr[i]);
+        }
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            if (min == arr[i]) {
+                count++;
+            }
+        }
+        sb.append(min).append(" ").append(count);
+        System.out.println(sb);
+
+    }
 
 }
