ec1a9f8faeb6b98a2062a76924fdf34300317825
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-12 13:36:53 +0900

    [Gold III] Title: 수들의 합 8, Time: 664 ms, Memory: 69472 KB -BaekjoonHub

--- 백준/Gold/25332. 수들의 합 8/수들의 합 8.java
+++ 백준/Gold/25332. 수들의 합 8/수들의 합 8.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,57 +8,52 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_007;
-	static int n;
-	static int m;
-	static List<int[]> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Long, Integer> map = new HashMap<>();
-	static long[] arr;
-	static long[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 150_000;
-	static String s;
-	static int result = 0;
+    static int INF = 1_000_000_007;
+    static int n;
+    static int r;
+    static int m;
+    static int c;
+    static int x;
+    static int[] arr;
+    static long[] tree;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		n = Integer.parseInt(br.readLine());
-		arr = new long[n + 1];
-		arr2 = new long[n + 1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr2[i] = Integer.parseInt(st.nextToken());
-		}
-		long[] arr3 = new long[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr3[i] = arr3[i-1] + arr2[i] - arr[i];
-		}
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<int[]>[] list;
 
-		long count = 0;
-		for (int i = 1; i <= n; i++) {
-			if (arr3[i]==0) count++;
-			int tmp = map.getOrDefault(arr3[i],0);
-			count+= tmp;
-			map.put(arr3[i], tmp + 1);
+    static long count = 0;
+    static int[] arr2;
+    static int[][] dp;
+    static long[] visit;
 
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		System.out.println(count);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] -= Integer.parseInt(st.nextToken());
+        }
+        Map<Long, Integer> map = new HashMap<>();
 
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += arr[i];
+            if (sum == 0) {
+                count++;
+            }
+            count += map.getOrDefault(sum, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        System.out.println(count);
 
-	}
-/*
-3
-3 1 2
-2 1 4
- */
-
+    }
 
 }
