f3a237c49d90b6ab84e93b696d9e03df8e6a2b80
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-17 10:55:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 148 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,57 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static List<int[]> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 100_000;
-	static long[] dp;
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n + 1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+    static int[] arr;
+    static long[] tree;
+    static List<int[]>[] list;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
 
-		int start = 0;
-		int end = 0;
-		int sum = arr[start];
-		long count = 0;
-		while (end < n) {
+    static long count = 0;
+    static int[][][][] dp;
 
-			if (sum < m) {
-				sum += arr[++end];
-			} else if (sum > m) {
-				sum -= arr[start++];
-			} else {
-				count++;
-				sum += arr[++end];
-				sum -= arr[start++];
-			}
-		}
-		System.out.println(count);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
 
-	}
+        }
+        int start = 0;
+        int end = 0;
+        int sum = arr[0];
+
+        while (end < n) {
+
+            if (sum < m) {
+                end++;
+                sum += arr[end];
+            } else if (sum > m) {
+                sum -= arr[start];
+                start++;
+            } else {
+                count++;
+                end++;
+                sum += arr[end];
+                sum -= arr[start];
+                start++;
+            }
+        }
+        System.out.println(count);
+
+    }
 
 }
