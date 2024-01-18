7dd5b3c7b5086ccb6a558ad04aa154419d7c22eb
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-18 15:21:36 +0900

    [Gold V] Title: 용액, Time: 388 ms, Memory: 28820 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,62 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static List<Integer> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 100_000;
-	static long[] dp;
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int start;
-		int end;
-		int abs = Integer.MAX_VALUE;
-		int a = 0;
-		int b = 0;
-		for (int i = 0; i < n; i++) {
-			int tmp = arr[i];
-			start = i + 1;
-			end = n - 1;
-			while (start <= end) {
-				int mid = (start + end) / 2;
-				if (Math.abs(tmp + arr[mid]) < abs) {
-					a = tmp;
-					b = arr[mid];
-					abs = Math.abs(a + b);
-				}
+    static int[] arr;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
 
-				if (tmp + arr[mid] <= 0) {
-					start = mid + 1;
-				} else {
-					end = mid - 1;
-				}
-			}
+    static int count = 0;
+    static int[][][][] dp;
 
-		}
-		System.out.println(Math.min(a, b) + " " + Math.max(a, b));
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int start;
+        int end;
+        int abs = Integer.MAX_VALUE;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            start = i + 1;
+            end = n - 1;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (Math.abs(tmp + arr[mid]) < abs) {
+                    a = tmp;
+                    b = arr[mid];
+                    abs = Math.abs(a + b);
+                }
+
+                if (tmp + arr[mid] <= 0) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        System.out.println(Math.min(a, b) + " " + Math.max(a, b));
+    }
+
 
 }
