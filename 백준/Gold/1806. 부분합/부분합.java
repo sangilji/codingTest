828cd04a11dfcc7924a3a2a829126f59ea17c7d7
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-23 09:55:44 +0900

    [Gold IV] Title: 부분합, Time: 276 ms, Memory: 23828 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,45 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-        int a = 0;
-        if (numbers[a]>=s){
-            System.out.println(1);
-            return;
-        }
-        if (Arrays.stream(numbers).sum()<s){
-            System.out.println(0);
-            return;
-        }
-        int sum = numbers[a];
-        int len = 100001;
-        for (int i = 1; i < n; i++) {
-            sum += numbers[i];
-            if (sum >= s) {
-                while (sum>=s){
-                    sum -= numbers[a++];
-                }
-                len = Math.min(len,i-a+2);
-            }
-        }
-        System.out.println(len);
-    }
+	static int MOD = 1_000_000;
+	static int n;
+	static int m;
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> list2 = new ArrayList<>();
+	static Map<Long, Integer> map = new HashMap<>();
+	static long[] prefix_gcd;
+	static long[] suffix_gcd;
+	static int[] arr;
+	static long[] tree;
+	static long[] lazy;
+	static int SIZE = 150_000;
+	static String s;
+	static int result = 0;
+	static int[] dp;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int start = 0;
+		int end = 0;
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		int count = 0;
+		while (true) {
+			if (end >= n) {
+				if (sum < m || start==end) {
+					break;
+				}else{
+					min = Math.min(min, count);
+					sum -= arr[start++];
+					count--;
+				}
+				continue;
+			}
+			if (sum < m) {
+				sum += arr[end++];
+				count++;
+			} else {
+				min = Math.min(min, count);
+				sum -= arr[start++];
+				count--;
+			}
+		}
+		if (sum >= m) {
+			min = Math.min(min, count);
+		}
+		if (min == Integer.MAX_VALUE) {
+			System.out.println(0);
+			return;
+		}
+		System.out.println(min);
+	}
 
 
 
