0ad7536147ef458ae7c710a54a7d74210a7585e8
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-26 10:04:47 +0900

    [Gold V] Title: 작은 벌점, Time: 288 ms, Memory: 22136 KB -BaekjoonHub

--- 백준/Gold/16498. 작은 벌점/작은 벌점.java
+++ 백준/Gold/16498. 작은 벌점/작은 벌점.java
@@ -1,65 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static List<Long> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 100_000;
-	static long[] dp;
-	static StringBuilder sb = new StringBuilder();
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-	public static void main(String[] args) throws IOException {
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-		int[] arr2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-		int[] arr3 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-		int result = Integer.MAX_VALUE;
-		for (int i = 0; i < A; i++) {
-			for (int j = 0; j < B; j++) {
-				int a = arr[i];
-				int b = arr2[j];
-				int start = 0;
-				int end = C - 1;
-				while (start <= end) {
-					int mid = (start + end) >> 1;
-					int c = arr3[mid];
-					int min = Math.min(a, Math.min(b, c));
-					int max = Math.max(a, Math.max(b, c));
-					int tmp = max - min;
-					if (tmp < result) {
-						result = tmp;
-					}
-					if (max == c) {
-						end = mid - 1;
-					} else {
-						start = mid + 1;
-					}
-				}
-			}
-		}
-		System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] A = new int[a];
+        int[] B = new int[b];
+        int[] C = new int[c];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < c; i++) {
+            C[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(C);
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                int start = 0;
+                int end = c - 1;
+                while (start <= end) {
+                    int mid = (start + end) >> 1;
+                    int max = Math.max(A[i], Math.max(B[j], C[mid]));
+                    int min = Math.min(A[i], Math.min(B[j], C[mid]));
+                    result = Math.min(result, max - min);
+                    if (C[mid] == min) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
 
-	}
+    }
+
 
 }
