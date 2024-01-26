def413b15e8a452723906ee17e609259f067c9fb
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-26 09:36:18 +0900

    [Silver III] Title: 이상한 술집, Time: 180 ms, Memory: 17280 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.java
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -1,50 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n;
-	static int m;
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-	static long count = 0;
-	static int[] arr;
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
 
-	static String s;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		long start = 0;
-		long end = Integer.MAX_VALUE;
-		long result = 0;
-		while (start <= end) {
-			long mid = (start + end) >> 1;
-			if (check(mid)) {
-				start = mid + 1;
-				result = mid;
-			} else {
-				end = mid - 1;
-			}
-		}
-		System.out.println(result);
+        }
+        long start = 1;
+        long end = Integer.MAX_VALUE;
+        long result = 0;
+        while (start <= end) {
+            long mid = (start + end) >> 1;
+            if (check(mid)) {
+                result = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(result);
 
-	}
+    }
 
-	private static boolean check(long value) {
-		long count = 0;
-		for (int i = 0; i < n; i++) {
-			count += arr[i] / value;
-		}
-		return count >= m;
-	}
+    private static boolean check(long mid) {
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            count += arr[i] / mid;
+        }
+        return count >= m;
+    }
+
 
 }
