19cc03a5b69a316159b6f9cf6454bec6bcd2603b
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-26 09:37:28 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17204 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,61 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static List<Long> list = new ArrayList<>();
-	static List<int[]> list2 = new ArrayList<>();
-	static Map<Integer, Integer> map = new HashMap<>();
-	static int[] arr;
-	static int[] arr2;
-	static long[] tree;
-	static long[] lazy;
-	static int SIZE = 100_000;
-	static long[] dp;
-	static StringBuilder sb = new StringBuilder();
+    static int INF = 1_000_000_007;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-	public static void main(String[] args) throws IOException {
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		long start = 1;
-		long end = Integer.MAX_VALUE;
-		long result = 0;
-		while (start <= end) {
-			long mid = (start + end) >> 1;
-			if (check(mid)) {
-				result = mid;
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
-		}
-		System.out.println(result);
+        }
+        long start = 1;
+        long end = Integer.MAX_VALUE;
+        long result = 0;
+        while (start <= end) {
+            long mid = (start + end) >> 1;
+            if (check(mid)) {
+                result = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(result);
 
-	}
+    }
 
-	private static boolean check(long num) {
-		long result = 0;
-		for (int i = 0; i < n; i++) {
-			result += arr[i] / num;
-		}
-		return result >= m;
-	}
+    private static boolean check(long mid) {
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            count += arr[i] / mid;
+        }
+        return count >= m;
+    }
+
 
 }
