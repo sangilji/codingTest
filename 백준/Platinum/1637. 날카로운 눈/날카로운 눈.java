1ce8b57b9c0d37157bd44f5c2e9c4b6e760937b0
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-29 11:44:41 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 316 ms, Memory: 23172 KB -BaekjoonHub

--- 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
@@ -6,54 +6,75 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
     static int n;
     static int m;
-    static int[][] arr;
+    static int k;
 
-    static List<Integer> list;
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        arr = new int[n][3];
+        arr1 = new int[n][3];
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        long start = 0;
+        long start = 1;
         long end = Integer.MAX_VALUE;
-        boolean b = false;
+        long result = -1;
         while (start <= end) {
             long mid = (start + end) >> 1;
-            if (check(mid)%2==0) {
-                start = mid + 1;
-            } else {
+            if (check(mid) % 2 == 1) {
+                result = mid;
                 end = mid - 1;
-                b = true;
+            } else {
+                start = mid + 1;
             }
         }
-        if (!b) {
+        if (result == -1) {
             System.out.println("NOTHING");
             return;
         }
-        long sum1 = check(start);
-        long sum2 = check(start-1);
-        System.out.println(start +" " +(sum1-sum2));
+        sb.append(result).append(" ").append(check(result) - check(result - 1));
+        System.out.println(sb);
+
+
     }
 
     private static long check(long mid) {
         long count = 0;
         for (int i = 0; i < n; i++) {
-            if (mid >= arr[i][0]) { //  a +kb <=c
-                long k = Math.min(arr[i][1], mid) - arr[i][0];
-                count += 1 + (k / arr[i][2]);
+            if (mid >= arr1[i][0]) {// a + kb <=c
+                count += 1 + ((Math.min(arr1[i][1], mid) - arr1[i][0]) / arr1[i][2]);
             }
         }
         return count;
     }
+
+
 }
