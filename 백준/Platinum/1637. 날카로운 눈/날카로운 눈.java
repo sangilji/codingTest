4b194335809ae56bf4222b9b0eee1693a50a9c97
지상일 <88275989+sangilji@users.noreply.github.com>
2024-01-29 11:42:33 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 336 ms, Memory: 24364 KB -BaekjoonHub

--- 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
@@ -1,15 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int n;
+    static int m;
     static int[][] arr;
+
+    static List<Integer> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         n = Integer.parseInt(br.readLine());
         arr = new int[n][3];
         for (int i = 0; i < n; i++) {
@@ -18,51 +24,36 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        boolean b = false;
-        long start = 1;
-        long last = Integer.MAX_VALUE;
-        long mid = 0;
-        while (start <= last) {
-            mid = (start + last) / 2;
 
-            long sum = 0;
-            for (int i = 0; i < n; i++) {
-                if (mid >= arr[i][0]) {
-                    long end = Math.min(arr[i][1], mid) - arr[i][0];
-                    sum += 1 + (end / arr[i][2]);
-                }
-            }
-            if (sum % 2 == 0) {
+        long start = 0;
+        long end = Integer.MAX_VALUE;
+        boolean b = false;
+        while (start <= end) {
+            long mid = (start + end) >> 1;
+            if (check(mid)%2==0) {
                 start = mid + 1;
             } else {
-                last = mid - 1;
+                end = mid - 1;
                 b = true;
             }
         }
         if (!b) {
             System.out.println("NOTHING");
-        } else {
-            long sum1 = 0;
-            for (int i = 0; i < n; i++) {
-                if (start >= arr[i][0]) {
-                    long end = Math.min(arr[i][1], start) - arr[i][0];
-                    sum1 += 1 + (end / arr[i][2]);
-                }
-            }
-
-            long sum2 = 0;
-            for (int i = 0; i < n; i++) {
-                if (start >= arr[i][0]) {
-                    long end = Math.min(arr[i][1], start - 1) - arr[i][0];
-                    sum2 += 1 + (end / arr[i][2]);
-                }
-            }
-
-            System.out.println(start + " " + (sum1 - sum2));
-
+            return;
         }
-
+        long sum1 = check(start);
+        long sum2 = check(start-1);
+        System.out.println(start +" " +(sum1-sum2));
     }
 
-
+    private static long check(long mid) {
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            if (mid >= arr[i][0]) { //  a +kb <=c
+                long k = Math.min(arr[i][1], mid) - arr[i][0];
+                count += 1 + (k / arr[i][2]);
+            }
+        }
+        return count;
+    }
 }
