154230cb234db07085c2031bfd64461bbf34f103
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-01 17:43:30 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 176 ms, Memory: 16480 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -2,42 +2,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n;
-	static int m;
-	static List<String> list = new ArrayList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		dfs(0, "");
-		list.sort(String::compareTo);
-		try {
-			System.out.println(list.get(m - 1));
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-		} catch (Exception e) {
-			System.out.println(-1);
-		}
-	}
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<String> list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
+    static StringBuilder sb = new StringBuilder();
 
-	private static void dfs(int depth, String s) {
-		if (depth > n) {
-			return;
-		}
-		if (depth == n) {
-			list.add(s.substring(0, s.length() - 1));
-			return;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        recur(0, "");
+        list.sort(String::compareTo);
+        if (list.size() < m) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(String.join("+", list.get(m - 1).split("")));
 
-		for (int i = 1; i <= 3; i++) {
-			dfs(depth+i,s+i+"+");
-		}
-	}
+    }
+
+    private static void recur(int depth, String s) {
+        if (depth > n) {
+            return;
+        }
+        if (depth == n) {
+            list.add(s);
+            return;
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            recur(depth + i, s + i);
+        }
+    }
+
 
 }
