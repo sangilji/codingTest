987d3e5d8d20ea2a08c25dff270b0e7bc061b715
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-02 13:50:00 +0900

    [Silver II] Title: 차이를 최대로, Time: 148 ms, Memory: 15208 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -6,62 +6,70 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1000000000;
-	static int n;
-	static int r;
-	static int m;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-	static int[] arr;
-	static int[] arr2;
-	static List<Integer>[] graph;
-	static List<Integer>[] list;
-	static int[] visit;
-	static int[] in;
-	static int[] out;
-	static int[] tree;
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<String> list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = -1;
+    static int[][] dp;
+    static StringBuilder sb = new StringBuilder();
 
-	static int count = 0;
-	static long[][] dp;
-	static long max = Long.MIN_VALUE;
-	static long min = Long.MAX_VALUE;
-	static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        arr2 = new int[n];
+        visit = new int[n];
+        recur(0);
+        System.out.println(count);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
+    private static void recur(int depth) {
+        if (depth == n) {
+            int tmp = 0;
+            for (int i = 0; i < n - 1; i++) {
+                tmp += Math.abs(arr2[i] - arr2[i + 1]);
+            }
+            count = Math.max(tmp, count);
+            return;
+        }
 
-		arr = new int[n];
-		arr2 = new int[n];
-		visit = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == 1) {
+                continue;
+            }
+            visit[i] = 1;
+            arr2[depth] = arr[i];
+            recur(depth + 1);
+            visit[i] = 0;
+        }
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		recur(0);
-		System.out.println(max);
-	}
+    }
 
-	private static void recur(int depth) {
-
-		if (depth == n) {
-			int sum = 0;
-			for (int i = 1; i < n; i++) {
-				sum += Math.abs(arr2[i - 1] - arr2[i]);
-			}
-			max = Math.max(sum,max);
-			return;
-		}
-		for (int i = 0; i < n; i++) {
-			if (visit[i] == 1) {
-				continue;
-			}
-			visit[i]=1;
-			arr2[depth] = arr[i];
-			recur(depth + 1);
-			visit[i]=0;
-		}
-	}
 
 }
