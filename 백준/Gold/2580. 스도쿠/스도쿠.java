f4043ad71a156b87b5b44df77d6fa8f176191265
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-08 09:29:23 +0900

    [Gold IV] Title: 스도쿠, Time: 404 ms, Memory: 19496 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,70 +1,91 @@
-
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] sdoku;
-    public static void main(String[] args) {
-        sdoku = new int[9][9];
-        Scanner s = new Scanner(System.in);
-        for (int i = 0; i < 9; i++) {
-            for(int j=0;j<9;j++){
-                sdoku[i][j]=s.nextInt();
+
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = 9;
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        f(0,0);
+        recur(0, 0);
+    }
+
+    static boolean b = false;
+
+    private static void recur(int x, int y) {
+        if (b) {
+            return;
+        }
+        if (y == 9) {
+            x += 1;
+            y = 0;
+        }
+        if (x == 9) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    sb.append(arr[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb);
+            b = true;
+            return;
+        }
+
+        if (arr[x][y] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                if (check(x, y, i)) {
+                    arr[x][y] = i;
+                    recur(x, y + 1);
+                    arr[x][y] = 0;
+                }
+            }
+        } else {
+            recur(x, y + 1);
+        }
 
     }
 
-    private static void f(int a, int b) {
-        if(b==9){
-            f(a+1, 0);
-            return;
-        }
-        if(a==9){
-            for(int i =0;i<9;i++){
-                for(int j=0;j<9;j++){
-                    System.out.print(sdoku[i][j] + " ");
-                }
-                System.out.println();
-            }
-            System.exit(0);
-
-        }
-        if(sdoku[a][b]==0){
-            for(int i =1;i<=9;i++){
-                if(possibility(a,b,i)){
-                    sdoku[a][b]=i;
-                    f(a, b + 1);
-                }
-            }
-            sdoku[a][b] = 0;
-            return;
-        }
-
-        f(a,b+1);
-    }
-
-    private static boolean possibility(int a, int b, int value) {
+    private static boolean check(int x, int y, int num) {
         for (int i = 0; i < 9; i++) {
-            if (sdoku[a][i] == value) {
+            if (arr[x][i] == num || arr[i][y] == num) {
                 return false;
             }
         }
 
-        for (int i = 0; i < 9; i++) {
-            if (sdoku[i][b] == value) {
-                return false;
-            }
-        }
+        int row = (x / 3) * 3;
+        int col = (y / 3) * 3;
 
-
-        int set_row = (a / 3) * 3;
-        int set_col = (b / 3) * 3;
-
-        for (int i = set_row; i < set_row + 3; i++) {
-            for (int j = set_col; j < set_col + 3; j++) {
-                if (sdoku[i][j] == value) {
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                if (arr[i][j] == num) {
                     return false;
                 }
             }
@@ -72,5 +93,6 @@ public class Main {
 
         return true;
     }
+
 
 }
