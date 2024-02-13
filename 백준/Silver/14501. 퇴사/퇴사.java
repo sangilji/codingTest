b81964337885cc94a05569efa9b0a094d164afa3
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-13 09:32:53 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -6,57 +6,56 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int MOD = 1000000000;
-	static int n;
-	static int r;
-	static int m;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	static int[][] arr;
-	static int[] arr2;
-	static List<Integer>[] graph;
-	static List<Integer>[] list;
-	static int[] visit;
-	static int[] in;
-	static int[] out;
-	static int[] tree;
+    static int[][] arr;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer> list;
+    static int[] result;
 
-	static int count = 0;
-	static long[][] dp;
-	static long max = Long.MIN_VALUE;
-	static long min = Long.MAX_VALUE;
-	static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        recur(0, 0);
+        System.out.println(count);
 
-		arr = new int[n][2];
 
+    }
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 2; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		recur(0, 0);
-		System.out.println(max);
-	}
+    private static void recur(int depth, int sum) {
 
-	private static void recur(int depth, int sum) {
+        if (depth > n) {
+            return;
+        }
+        if (depth == n) {
+            count = Math.max(count, sum);
+            return;
+        }
 
-		if (depth > n) {
-			return;
-		}
-		if (depth == n) {
-			max = Math.max(sum, max);
-			return;
-		}
+        recur(depth + arr[depth][0], sum + arr[depth][1]);
+        recur(depth + 1, sum);
 
-		recur(depth + arr[depth][0], sum + arr[depth][1]);
-		recur(depth + 1, sum);
+    }
 
-	}
 
 }
