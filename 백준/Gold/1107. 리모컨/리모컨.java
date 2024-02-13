dbf85467e09120256ee0434fafdd13329134710d
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-13 09:12:12 +0900

    [Gold V] Title: 리모컨, Time: 332 ms, Memory: 109248 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,46 +1,65 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static boolean[] breakButton;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int channel = s.nextInt();
-		if (channel == 100) {
-			System.out.println(0);
-			return;
-		}
-		int n = s.nextInt();
-		breakButton = new boolean[10];
-		for (int i = 0; i < n; i++) {
-			int m = s.nextInt();
-			breakButton[m] = true;
-		}
-		int answer = Math.abs(100 - channel);
-		for (int i = 0; i < 1_000_000; i++) {
-			if (check(String.valueOf(i))) {
-				int tmp = Math.abs(channel - i) + String.valueOf(i).length();
-				if(answer > tmp){
-					answer = tmp;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-				}
+    static int[] arr;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = INF;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer> list;
+    static int[] result;
 
-			}
-		}
-		if(answer == 100 - channel){
-			System.out.println(answer);
-		} else {
-			System.out.println(answer);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        arr = new int[10];
+        if (m != 0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                arr[tmp] = 1;
+            }
+        }
+        if (n == 100) {
+            System.out.println(0);
+            return;
+        }
+        int answer = Math.abs(n - 100);
 
-	private static boolean check(String i) {
-		for (String s : i.split("")) {
-			if (breakButton[Integer.parseInt(s)]) {
-				return false;
-			}
-		}
-		return true;
-	}
+        for (int i = 0; i < 1000000; i++) {
+            if (check(String.valueOf(i))) {
+                int tmp = Math.abs(n - i) + String.valueOf(i).length();
+                answer = Math.min(answer, tmp);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean check(String i) {
+        for (char c : i.toCharArray()) {
+            if (arr[c - '0'] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
