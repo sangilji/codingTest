b6ba2e58fa02858d7a68ee3c9ef7ad7553b1ebe8
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-19 15:19:24 +0900

    [Gold V] Title: 내려가기, Time: 580 ms, Memory: 65124 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,52 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[][] map = new int[n][3];
+
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = INF;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<int[]>[] list;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][3];
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                map[i][j] = s.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int[][] minDp = new int[n][3];
-        int[][] maxDp = new int[n][3];
-        for (int i = 0; i < 3; i++) {
-            minDp[0][i] = map[0][i];
-            maxDp[0][i] = map[0][i];
+        dp = new int[n][3];
+        dp2 = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp2[i], -1);
         }
-        for (int i = 1; i < n; i++) {
-            Arrays.fill(minDp[i], Integer.MAX_VALUE);
-        }
-        for (int i = 1; i < n; i++) {
-            maxDp[i][1] = Math.max(maxDp[i - 1][0] + map[i][1], maxDp[i][1]);
-            maxDp[i][0] = Math.max(maxDp[i - 1][0] + map[i][0], maxDp[i][0]);
-
-            maxDp[i][2] = Math.max(maxDp[i - 1][1] + map[i][2], maxDp[i][2]);
-            maxDp[i][1] = Math.max(maxDp[i - 1][1] + map[i][1], maxDp[i][1]);
-            maxDp[i][0] = Math.max(maxDp[i - 1][1] + map[i][0], maxDp[i][0]);
-
-            maxDp[i][2] = Math.max(maxDp[i - 1][2] + map[i][2], maxDp[i][2]);
-            maxDp[i][1] = Math.max(maxDp[i - 1][2] + map[i][1], maxDp[i][1]);
-
-            minDp[i][1] = Math.min(minDp[i - 1][0] + map[i][1], minDp[i][1]);
-            minDp[i][0] = Math.min(minDp[i - 1][0] + map[i][0], minDp[i][0]);
-
-            minDp[i][2] = Math.min(minDp[i - 1][1] + map[i][2], minDp[i][2]);
-            minDp[i][1] = Math.min(minDp[i - 1][1] + map[i][1], minDp[i][1]);
-            minDp[i][0] = Math.min(minDp[i - 1][1] + map[i][0], minDp[i][0]);
-
-            minDp[i][2] = Math.min(minDp[i - 1][2] + map[i][2], minDp[i][2]);
-            minDp[i][1] = Math.min(minDp[i - 1][2] + map[i][1], minDp[i][1]);
+        System.out.println(recur(0, 1) + " " + recur2(0, 1));
 
 
-        }
-        System.out.println(Arrays.stream(maxDp[n - 1])
-                .max()
-                .getAsInt() + " " + Arrays.stream(minDp[n - 1])
-                .min()
-                .getAsInt());
     }
+
+    private static int recur2(int depth, int prev) {
+        if (depth == n) {
+            return 0;
+        }
+        if (dp2[depth][prev] != -1) {
+            return dp2[depth][prev];
+        }
+        int tmp = 123123123;
+        for (int i = 0; i < 3; i++) {
+            if (Math.abs(prev - i) <= 1) {
+                tmp = Math.min(tmp, recur2(depth + 1, i) + arr[depth][i]);
+            }
+        }
+        return dp2[depth][prev] = tmp;
+    }
+
+    private static int recur(int depth, int prev) {
+        if (depth == n) {
+            return 0;
+        }
+        if (dp[depth][prev] != -1) {
+            return dp[depth][prev];
+        }
+        int tmp = 0;
+        for (int i = 0; i < 3; i++) {
+            if (Math.abs(prev - i) <= 1) {
+                tmp = Math.max(tmp, recur(depth + 1, i) + arr[depth][i]);
+            }
+        }
+        return dp[depth][prev] = tmp;
+
+    }
+
+
 }
