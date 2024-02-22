9c2c9b453b52f9a788c43089dd2b5a92624b87b3
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-22 14:25:45 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 18720 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -5,38 +5,46 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int[][] dp;
 
-	static int[] arr;
-	static int[] tree;
-	static List<Integer>[] graph;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	static long min = Integer.MAX_VALUE;
+    static int[] arr;
+    static int[][] sum;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<int[]>[] list;
+    static int[] result;
+    static int min = INF;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		String a = " " + br.readLine();
-		String b = " " + br.readLine();
-		n = a.length();
-		m = b.length();
-		dp = new int[n][m];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String a = br.readLine();
+        String b = br.readLine();
+        n = a.length();
+        m = b.length();
+        dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
 
-		for (int i = 1; i < n; i++) {
-			for (int j = 1; j < m; j++) {
-				if (a.charAt(i) == b.charAt(j)) {
-					dp[i][j] = dp[i - 1][j - 1] + 1;
-				}else{
-					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-				}
-			}
-		}
-		System.out.println(dp[n-1][m-1]);
-
-	}
 
 }
