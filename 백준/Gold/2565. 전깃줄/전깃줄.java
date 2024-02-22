b12f41a62bf55dc994152cd863aed21d89d78598
지상일 <88275989+sangilji@users.noreply.github.com>
2024-02-22 18:32:08 +0900

    [Gold V] Title: 전깃줄, Time: 132 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -7,40 +7,50 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int[] dp;
-	static int[][] arr;
-	static int[] tree;
-	static List<Integer>[] graph;
 
-	static long min = Integer.MAX_VALUE;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n + 1][2];
-		for (int i = 1; i <= n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 2; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+    static int[][] arr;
+    static int[][] sum;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<int[]>[] list;
+    static int[] result;
+    static int min = INF;
 
-		dp = new int[n + 1];
-		int max = 0;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[i][1] > arr[j][1]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-					max = Math.max(max, dp[i]);
-				}
-			}
-		}
-		System.out.println(n-max);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][2];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+        dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        System.out.println(n - Arrays.stream(dp).max().getAsInt());
+
+    }
+
 
 }
