c7055be9e43f4b56c73765dd365d270fdda35adb
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-11 09:30:59 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,68 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static List<Integer>[] nodes;
-	static int n;
-	static int start;
-	static int end;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
+    static int[][] arr;
+    static int[][] sum;
+    static int[] visit;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer>[] list;
+    static int[] result;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		nodes = new List[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			nodes[i] = new ArrayList<>();
-		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(br.readLine());
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			nodes[a].add(b);
-			nodes[b].add(a);
-		}
-		bfs(start, end);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(br.readLine());
+        visit = new int[n + 1];
+        list = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+        Arrays.fill(visit, -1);
+        visit[s] = 0;
+        dfs(s);
+        System.out.println(visit[e]);
+    }
 
-	}
+    private static void dfs(int cur) {
+        for (int i = 0; i < list[cur].size(); i++) {
+            if (visit[list[cur].get(i)] > 0) {
+                continue;
+            }
+            visit[list[cur].get(i)] = visit[cur] + 1;
+            dfs(list[cur].get(i));
+        }
+    }
 
-	private static void bfs(int start, int end) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {start, 0});
-		boolean[] visit = new boolean[n + 1];
-		visit[start] = true;
-
-		while (!q.isEmpty()) {
-			int[] tmp = q.poll();
-			if (tmp[0] == end) {
-				System.out.println(tmp[1]);
-				return;
-			}
-			for (int i = 0; i < nodes[tmp[0]].size(); i++) {
-				int next = nodes[tmp[0]].get(i);
-				if (visit[next]) {
-					continue;
-				}
-				visit[next] = true;
-				q.add(new int[] {next, tmp[1] + 1});
-			}
-		}
-
-		System.out.println(-1);
-
-	}
 
 }
