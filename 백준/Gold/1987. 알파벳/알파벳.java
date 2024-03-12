cc49b2a7c6d89ff9147be960f69d6a751a7bff12
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-12 11:06:37 +0900

    [Gold IV] Title: 알파벳, Time: 788 ms, Memory: 15284 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,51 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int k;
 
-	static char[][] arr;
-	static int[] alpha;
-	static int[] dx = {0, -1, 0, 1};
-	static int[] dy = {1, 0, -1, 0};
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	static int count = 0;
+    static int[][] arr;
+    static int sum;
+    static int[] visit;
+    static int[] price;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<int[]> list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new char[n][];
-		alpha = new int[26];
-		for (int i = 0; i < n; i++) {
-			arr[i] = br.readLine().toCharArray();
-		}
-		System.out.println(dfs(0, 0));
-	}
+    static int[] result;
 
-	private static int dfs(int x, int y) {
-		alpha[arr[x][y] - 'A'] = 1;
-		int count = 0;
-		for (int i = 0; i < 4; i++) {
-			int tx = x + dx[i];
-			int ty = y + dy[i];
-			if (tx < 0 || ty < 0 || tx >= n || ty >= m || alpha[arr[tx][ty] - 'A'] == 1) {
-				continue;
-			}
-			count = Math.max(count, dfs(tx, ty));
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		alpha[arr[x][y] - 'A'] = 0;
-		return count + 1;
+        arr = new int[n][m];
+        visit = new int[26];
+        for (int i = 0; i < n; i++) {
+            char[] tmp = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = tmp[j] - 'A';
+            }
+        }
+        System.out.println(dfs(0, 0));
 
-	}
+    }
+
+    private static int dfs(int x, int y) {
+        visit[arr[x][y]] = 1;
+
+        int tmp = 0;
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx < 0 || ty < 0 || tx >= n || ty >= m || visit[arr[tx][ty]] == 1) {
+                continue;
+            }
+            tmp = Math.max(tmp, dfs(tx, ty));
+        }
+        visit[arr[x][y]] = 0;
+        return tmp + 1;
+    }
+
 
 }
