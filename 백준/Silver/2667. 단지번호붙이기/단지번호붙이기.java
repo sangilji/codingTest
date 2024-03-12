73868ae3cc3bd5e5327c589824a3269197cc4657
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-12 11:19:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,69 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int k;
-	static int[][] dp;
 
-	static int[][] arr;
-	static List<Integer>[] list;
-	static int[][] visit;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	static int count = 0;
+    static int[][] arr;
+    static int sum;
+    static int[][] visit;
+    static int[] price;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer> list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+    static int[] result;
 
-		n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
 
-		arr = new int[n][n];
-		visit = new int[n][n];
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            char[] tmp = br.readLine().toCharArray();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = tmp[j] - '0';
+            }
+        }
+        visit = new int[n][n];
+        list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visit[i][j] == 1 || arr[i][j] == 0) {
+                    continue;
+                }
+                list.add(dfs(i, j));
+            }
+        }
+        sb.append(list.size()).append("\n");
 
-		for (int i = 0; i < n; i++) {
-			String[] tmp = br.readLine().split("");
-			for (int j = 0; j < n; j++) {
-				arr[i][j] = Integer.parseInt(tmp[j]);
-			}
-		}
-		PriorityQueue<Integer> q = new PriorityQueue<>(Integer::compareTo);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (visit[i][j] == 0 && arr[i][j] == 1) {
-					q.add(dfs(i, j));
-					count++;
-				}
-			}
-		}
-		sb.append(count).append("\n");
-		while (!q.isEmpty()) {
-			sb.append(q.poll()).append("\n");
-		}
-		System.out.println(sb);
-	}
+        list.sort(Integer::compareTo);
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-	private static int dfs(int x,int y) {
-		visit[x][y] = 1;
-		int tmp = 1;
-		for (int i = 0; i < 4; i++) {
-			int tx = x + dx[i];
-			int ty = y + dy[i];
-			if (tx < 0 || ty < 0 || tx >= n || ty >= n || visit[tx][ty] == 1 || arr[tx][ty] == 0) {
-				continue;
-			}
-			tmp += dfs(tx, ty);
-		}
-		return tmp;
-	}
+    private static int dfs(int x, int y) {
+        visit[x][y] = 1;
+        int tmp = 1;
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx < 0 || ty < 0 || tx >= n || ty >= n || arr[tx][ty] == 0 || visit[tx][ty] == 1) {
+                continue;
+            }
+            tmp += dfs(tx, ty);
+        }
+        return tmp;
+    }
+
 
 }
