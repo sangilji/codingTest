b6b2ba22553147a92dd60bf0362ee4486bf25872
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-13 15:30:48 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 16408 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,70 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int n;
-	static int m;
-	static int k;
 
-	static int[][] arr;
-	static int[][] visit;
-	static List<Integer> list = new ArrayList<>();
-	static List<Integer>[] graph;
-	static int[] alpha;
-	static int[] dx = {0, -1, 0, 1};
-	static int[] dy = {1, 0, -1, 0};
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	static int count = 0;
-	static Set<Integer> s = new HashSet<>();
-	static int x;
-	static int y;
+    static int[][] arr;
+    static int sum;
+    static int[] visit;
+    static int[] price;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer> list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		bfs();
+    static int[] result;
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        bfs();
+    }
 
-	private static void bfs() {
-		PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-		boolean[] visit = new boolean[100001];
+    private static void bfs() {
+        Queue<Integer> q = new ArrayDeque<>();
+        visit = new int[100001];
+        Arrays.fill(visit, -1);
+        q.add(n);
+        visit[n] = 0;
 
-		q.add(new int[] {n, 0});
-		while (!q.isEmpty()) {
-			int[] tmp = q.poll();
-			if (tmp[0] == m) {
-				System.out.println(tmp[1]);
-				return;
-			}
-			if (tmp[0] - 1 >= 0 && !visit[tmp[0] - 1]) {
-				visit[tmp[0] - 1] = true;
-				q.add(new int[] {tmp[0] - 1, tmp[1] + 1});
-			}
-			if (tmp[0] + 1 <= 100000 && !visit[tmp[0] + 1]) {
-				visit[tmp[0] + 1] = true;
-				q.add(new int[] {tmp[0] + 1, tmp[1] + 1});
-			}
-			if (tmp[0] * 2 <= 100000 && !visit[tmp[0] * 2]) {
-				visit[tmp[0] * 2] = true;
-				q.add(new int[] {tmp[0] * 2, tmp[1] + 1});
-			}
-		}
-	}
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
 
-	static int result = 0;
+            if (tmp == m) {
+                System.out.println(visit[tmp]);
+                return;
+            }
+            if (tmp * 2 <= 100000 && visit[tmp * 2] == -1) {
+                visit[tmp * 2] = visit[tmp] + 1;
+                q.add(tmp * 2);
+            }
+            if (tmp + 1 <= 100000 && visit[tmp + 1] == -1) {
+                visit[tmp + 1] = visit[tmp] + 1;
+                q.add(tmp + 1);
+            }
+            if (tmp - 1 >= 0 && visit[tmp - 1] == -1) {
+                visit[tmp - 1] = visit[tmp] + 1;
+                q.add(tmp - 1);
+            }
+        }
+    }
+
 
 }
