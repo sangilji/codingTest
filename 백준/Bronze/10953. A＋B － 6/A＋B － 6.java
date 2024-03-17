d4c623dab6ffe455652806b1e72eb9539ddc0b0a
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-17 22:54:48 +0900

    [Bronze III] Title: A+B - 6, Time: 136 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); // 테스트 케이스의 개수 T
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수 T
 
 		int sum = 0;
 		for (int i = 0; i < T; i++) {
-			String AB = sc.next();
+			String AB = br.readLine();
 			String[] arr = AB.split(",");
 
 			for (int j = 0; j < arr.length; j++) {
@@ -16,6 +17,5 @@ public class Main {
 			System.out.println(sum);
 			sum = 0;// 한번 더하고 나면 0으로 초기화 해주기
 		}
-		sc.close();
 	}
 }
