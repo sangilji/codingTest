25d3d7623e2782979d6a91b885023e4bbfa452b5
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-25 21:42:30 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 208 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int N = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < N; i++) {
+			String str = scan.nextLine();
+
+			System.out.println((str.charAt(0) + "").toUpperCase() + str.substring(1, str.length()));
+		}
+
+		scan.close();
+	}
+}
