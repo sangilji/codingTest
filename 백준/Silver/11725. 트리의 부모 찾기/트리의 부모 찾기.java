375b1551f640fe16158ed9ce632c43fa39acd83c
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-29 11:28:10 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 576 ms, Memory: 63820 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,62 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int INF = 2_000_000_000;
-	static int n;
-	static int m;
-	static int x;
-	static int y;
 
-	static int[] arr;
-	static int[][] visit;
-	static List<Integer> list = new ArrayList<>();
-	static List<Integer>[] graph;
-	static int[] parent;
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
-	static StringBuilder sb = new StringBuilder();
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+    static int sum;
+    static int[] visit;
+    static int[] parent;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr1;
+    static int count = 0;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer>[] list;
+    static List<Integer>[] list2;
 
-		StringTokenizer st;
+    static int[] result;
+    static Set<String> set = new HashSet<>();
 
-		n = Integer.parseInt(br.readLine());
-		graph = new List[n + 1];
-		parent = new int[n + 1];
-		for (int i = 0; i <= n; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		for (int i = 0; i < n - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph[a].add(b);
-			graph[b].add(a);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        list = new List[n + 1];
+        visit = new int[n + 1];
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+        dfs(1);
+        for (int i = 2; i <= n; i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.println(sb);
 
-		dfs(1, 0);
-		for (int i = 2; i <= n; i++) {
-			sb.append(parent[i]).append("\n");
-		}
-		System.out.println(sb);
+    }
 
-	}
+    private static void dfs(int cur) {
+        visit[cur] = 1;
 
-	private static void dfs(int cur, int prev) {
+        for (int i = 0; i < list[cur].size(); i++) {
+            if (visit[list[cur].get(i)] == 1) {
+                continue;
+            }
+            parent[list[cur].get(i)] = cur;
+            dfs(list[cur].get(i));
+        }
+    }
 
-		for (int i = 0; i < graph[cur].size(); i++) {
-			if (graph[cur].get(i) == prev) {
-				continue;
-			}
-			parent[graph[cur].get(i)] = cur;
-			dfs(graph[cur].get(i), cur);
-		}
-	}
 
 }
