4fa915793336bf15ce9a1c142375697ab39fad5b
지상일 <88275989+sangilji@users.noreply.github.com>
2024-03-29 18:15:42 +0900

    [Bronze II] Title: 정수의 개수, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.print(br.readLine().split(",").length);
+    }
+}
