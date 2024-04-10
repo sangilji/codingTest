2688e34721d6e6b374bd141196b8263572a61e9f
지상일 <88275989+sangilji@users.noreply.github.com>
2024-04-10 21:27:25 +0900

    [Bronze III] Title: 플러그, Time: 1240 ms, Memory: 311760 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		for(int i=0; i<cnt; i++) {
+			int num = sc.nextInt();
+			sum+=num;
+		}
+		System.out.println(sum-(cnt-1));
+	}
+}
