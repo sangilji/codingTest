8a6dce18e7d86ee9170fac8d601dcef7df324c04
지상일 <88275989+sangilji@users.noreply.github.com>
2024-04-12 16:29:14 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 380 ms, Memory: 20892 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int star = scanner.nextInt();
+
+     // 첫번째 for문
+    for(int i = 1 ; i <= star; i++) {
+      //공백찍기
+      for(int j = 1; j < i; j++) {
+        System.out.print(" ");
+      }
+      //별찍기
+      for(int k = (2*star)-(i*2-1); k > 0; k--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    scanner.close();
+  }
+}
