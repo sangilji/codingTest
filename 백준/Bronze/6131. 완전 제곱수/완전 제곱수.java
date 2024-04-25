0479f2576a59450d7212251f94b6bcb56e51b55d
지상일 <88275989+sangilji@users.noreply.github.com>
2024-04-25 23:27:42 +0900

    [Bronze III] Title: 완전 제곱수, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -1,32 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		int count = 0;
-		for (int i = 1; i <= 500; i++) {
-			for (int j = 1; j <= 500; j++) {
-				if (i * i == j * j + n) {
-					count++;
-				}
-			}
-		}
-		System.out.println(count);
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
 
-	}
+    static int[] arr;
+    static int[] tree;
+    static double sum;
+    static int[] visit;
+    static int[] parent;
+    static int[] size;
+    static int[] min;
+    static int[] max;
+    static int[] dx = {1, -1, 1, -1};
+    static int[] dy = {1, 1, -1, -1};
+    static int[] arr1;
+    static int count = 0;
+    static long[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static List<Integer>[] list;
+    static List<Integer>[] list2;
 
+    static int[] result;
+    static Set<String> set = new HashSet<>();
 
-
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 500; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i * i == j * j + n) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 
 }
