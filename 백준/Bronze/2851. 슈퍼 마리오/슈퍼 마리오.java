b64d0551be28eddd315f93c607c08c89ec44a813
지상일 <88275989+sangilji@users.noreply.github.com>
2024-04-25 23:33:21 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,44 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int INF = 1_000_000_000;
+
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
     static int n;
     static int m;
-    static List<Integer>[] list;
-    static int[] parent;
-    static int[] score;
-    static int[] cards;
+    static int k;
+
     static int[] arr;
-    static int[][][] dp;
-    static int[] sz;
+    static int[] tree;
+    static double sum;
     static int[] visit;
-    static long count;
+    static int[] parent;
+    static int[] size;
+    static int[] min;
+    static int[] max;
+    static int[] dx = {1, -1, 1, -1};
+    static int[] dy = {1, 1, -1, -1};
+    static int[] arr1;
+    static int count = 0;
+    static long[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static List<Integer>[] list;
+    static List<Integer>[] list2;
+
+    static int[] result;
+    static Set<String> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-
         arr = new int[10];
         for (int i = 0; i < 10; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int sum = arr[0];
-
-        for (int i = 1; i < 10; i++) {
-            if (Math.abs(100 - (sum + arr[i])) <= Math.abs(100 - sum)) {
-                sum += arr[i];
+        for (int i = 0; i < 10; i++) {
+            if (Math.abs(count + arr[i] - 100) <= Math.abs(count - 100)) {
+                count += arr[i];
             } else {
                 break;
             }
         }
-        System.out.println(sum);
+        System.out.println(count);
+
 
     }
-
 
 }
