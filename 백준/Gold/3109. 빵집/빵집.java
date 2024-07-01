e237131df2d75787089c00c03e4f210df20ff576
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-01 16:00:13 +0900

    [Gold II] Title: 빵집, Time: 1280 ms, Memory: 329780 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,63 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int r;
-    static int c;
-    static boolean[][] map;
+
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_009;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[][] sum;
+    static int[] visit;
+    static int[] price;
     static int[] dx = {-1, 0, 1};
-    static boolean[] visit;
+    static int[] dy = {1, 1, 1};
+    static int[][] arr1;
     static int count = 0;
-    static int currentX;
+    static int[] dp;
+    static StringBuilder sb = new StringBuilder();
+    static String[] s;
+    static List<Integer>[] list;
+    static int[] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        visit = new boolean[r];
-        map = new boolean[r][c];
-        for (int i = 0; i < r; i++) {
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
             String[] tmp = br.readLine().split("");
-            for (int j = 0; j < c; j++) {
-                if (tmp[j].equals("x")) {
-                    map[i][j] = true;
+            for (int j = 0; j < m; j++) {
+                if (tmp[j].equals(".")) {
+                    arr[i][j] = 0;
+                } else {
+                    arr[i][j] = 1;
                 }
             }
         }
-        for (int i = 0; i < r; i++) {
-            currentX = i;
+        for (int i = 0; i < n; i++) {
             dfs(i, 0);
-
         }
-
         System.out.println(count);
+
     }
 
-    private static void dfs(int x, int y) {
-        if (y == c - 1) {
-            visit[currentX] = true;
+    private static boolean dfs(int x, int y) {
+        arr[x][y] = 1;
+        if (y == m - 1) {
             count++;
+            return true;
         }
-
+        boolean tmp = false;
         for (int i = 0; i < 3; i++) {
-            if (visit[currentX]) {
-                return;
-            }
-            int tx = dx[i] + x;
-            int ty = y + 1;
-            if (tx < 0 || tx >= r || ty >= c || map[tx][ty]) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx < 0 || tx >= n || arr[tx][ty] == 1) {
                 continue;
             }
-            map[tx][ty] = true;
-            dfs(tx, ty);
+            tmp = dfs(tx, ty);
+            if (tmp) {
+                break;
+            }
         }
-        
-
+        return tmp;
     }
+
+
 }
