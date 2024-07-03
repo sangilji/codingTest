986d0c22183296d5cb6239b8bae667aa5d5c663c
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-03 10:52:15 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16660 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,35 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     static int INF = 1_000_000_000;
-    static int MOD = 1_000_000_009;
     static int n;
     static int m;
     static int k;
-
+    static int str;
     static int[][] arr;
-    static int sum;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][][] dp;
+    static List<Integer>[] list;
     static int[] visit;
-    static int[] price;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] arr1;
-    static int count = 0;
-    static int[] dp;
     static StringBuilder sb = new StringBuilder();
-    static String[] s;
-    static List<int[]>[] list;
-    static List<int[]>[] list2;
-
-    static int[] result;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,11 +26,11 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n][m];
+        arr = new int[n][];
         for (int i = 0; i < n; i++) {
             arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
+
         bfs();
 
 
@@ -49,29 +38,31 @@ public class Main {
 
     private static void bfs() {
         Queue<int[]> q = new ArrayDeque<>();
-        int[][] dist = new int[n][m];
+        int[][] visit = new int[n][m];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], -1);
+            Arrays.fill(visit[i], -1);
         }
+        visit[0][0] = 0;
         q.add(new int[]{0, 0});
-        dist[0][0] = 0;
-
         while (!q.isEmpty()) {
             int[] tmp = q.poll();
+
+            if (tmp[0] == n - 1 && tmp[1] == m - 1) {
+                System.out.println(visit[n - 1][m - 1]+1);
+                return;
+            }
 
             for (int i = 0; i < 4; i++) {
                 int tx = dx[i] + tmp[0];
                 int ty = dy[i] + tmp[1];
-                if (tx < 0 || ty < 0 || tx >= n || ty >= m || dist[tx][ty] != -1 || arr[tx][ty] == 0) {
+                if (tx < 0 || ty < 0 || tx >= n || ty >= m || visit[tx][ty] != -1 || arr[tx][ty]==0) {
                     continue;
                 }
-                dist[tx][ty] = dist[tmp[0]][tmp[1]] + 1;
+                visit[tx][ty] = visit[tmp[0]][tmp[1]] + 1;
                 q.add(new int[]{tx, ty});
             }
 
         }
-        System.out.println(dist[n - 1][m - 1]+1);
-
     }
 
 
