c6ae58d2a05a4b918e3a6509715f42ac69a98af8
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-03 10:46:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,52 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     static int INF = 1_000_000_000;
-    static int MOD = 1_000_000_009;
     static int n;
     static int m;
     static int k;
-
+    static int str;
     static int[][] arr;
-    static int sum;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][][] dp;
+    static List<Integer>[] list;
     static int[] visit;
-    static int[] price;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] arr1;
-    static int count = 0;
-    static int[] dp;
     static StringBuilder sb = new StringBuilder();
-    static String[] s;
-    static List<Integer> list;
-
-    static int[] result;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         bfs();
+
+
     }
 
     private static void bfs() {
         Queue<Integer> q = new ArrayDeque<>();
         visit = new int[100001];
         Arrays.fill(visit, -1);
-        q.add(n);
         visit[n] = 0;
-
+        q.add(n);
         while (!q.isEmpty()) {
             int tmp = q.poll();
 
@@ -54,17 +44,17 @@ public class Main {
                 System.out.println(visit[tmp]);
                 return;
             }
-            if (tmp * 2 <= 100000 && visit[tmp * 2] == -1) {
-                visit[tmp * 2] = visit[tmp] + 1;
-                q.add(tmp * 2);
+            if (tmp - 1 >= 0 && visit[tmp - 1] == -1) {
+                visit[tmp - 1] = visit[tmp] + 1;
+                q.add(tmp - 1);
             }
             if (tmp + 1 <= 100000 && visit[tmp + 1] == -1) {
                 visit[tmp + 1] = visit[tmp] + 1;
                 q.add(tmp + 1);
             }
-            if (tmp - 1 >= 0 && visit[tmp - 1] == -1) {
-                visit[tmp - 1] = visit[tmp] + 1;
-                q.add(tmp - 1);
+            if (tmp * 2 <= 100000 && visit[tmp * 2] == -1) {
+                visit[tmp * 2] = visit[tmp] + 1;
+                q.add(tmp * 2);
             }
         }
     }
