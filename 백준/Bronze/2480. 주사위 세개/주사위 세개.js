932aabefd0d1b53201c519babc75615aa00d7efe
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-04 12:48:13 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
-
-const numbers = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
-// const numbers = fs.readFileSync('input.txt').toString().split(' ').map(Number);
+const numbers = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
 
 const a = numbers[0];
 const b = numbers[1];
 const c = numbers[2];
 
-if(a==b && b==c){
-    console.log(10000+a*1000);
-}else if(a==b){
-    console.log(1000+a*100);
-}
-else if(a==c){
-    console.log(1000+a*100);
-    
-}
-else if(b==c){
-    console.log(1000+b*100);
-    
-}else{
-    let result = Math.max(a,b,c);
-    console.log(result*100);
+if (a==b && b==c) {
+    console.log(10000+(a*1000));
+} else if ((a==b && a!=c) || (a==c && a!=b)){
+    console.log(1000+(a*100))
+} else if ((b==a && b!= c) || (b==c && b!=a)){
+    console.log(1000+(b*100))
+} else {
+    let max = a;
+    if (max < b && c < b) {
+        max = b;
+        console.log(100*b);
+    } else if (max < c && b < c) {
+        max = c;
+        console.log(100*c);
+    } else  {
+        console.log(max*100);
+    }
 }
