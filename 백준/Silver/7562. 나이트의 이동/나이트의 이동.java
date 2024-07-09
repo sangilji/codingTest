a0d83c8b3587361379881182596e7386f38af0d8
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-09 09:41:46 +0900

    [Silver I] Title: 나이트의 이동, Time: 324 ms, Memory: 57480 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,27 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int MOD = 1_000_000_000;
     static int n;
-    static int r;
     static int m;
-    static int k;
-
-    static int[] arr;
+    static int[][] arr;
     static int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
     static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
-    static List<Integer>[] list;
-
-    static int count = 0;
     static int[] arr2;
-    static int[][] dp;
+    static int[][] arr1;
+
+    static long[] tree;
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,38 +26,42 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             n = Integer.parseInt(br.readLine());
+            arr = new int[n][n];
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            int lastX = Integer.parseInt(st.nextToken());
-            int lastY = Integer.parseInt(st.nextToken());
-            sb.append(bfs(x, y, lastX, lastY)).append("\n");
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            sb.append(bfs(x1, y1, x2, y2)).append("\n");
+
         }
         System.out.println(sb);
-
-
     }
 
-    private static int bfs(int x, int y, int lastX, int lastY) {
-        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
-        q.add(new int[]{x, y, 0});
-        boolean[][] visit = new boolean[n][n];
-        visit[x][y] = true;
+    private static int bfs(int x1, int y1, int x2, int y2) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] visit = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visit[i], -1);
+        }
+        q.add(new int[]{x1, y1});
+        visit[x1][y1] = 0;
         while (!q.isEmpty()) {
             int[] tmp = q.poll();
-            if (tmp[0] == lastX && tmp[1] == lastY) {
-                return tmp[2];
+            if (tmp[0] == x2 && tmp[1] == y2) {
+                return visit[x2][y2];
             }
             for (int i = 0; i < 8; i++) {
-                int tx = tmp[0] + dx[i];
-                int ty = tmp[1] + dy[i];
-                if (tx < 0 || ty < 0 || tx >= n || ty >= n || visit[tx][ty]) {
+                int tx = dx[i] + tmp[0];
+                int ty = dy[i] + tmp[1];
+                if (tx < 0 || ty < 0 || tx >= n || ty >= n || visit[tx][ty] != -1) {
                     continue;
                 }
-                visit[tx][ty] = true;
-                q.add(new int[]{tx, ty, tmp[2] + 1});
+                visit[tx][ty] = visit[tmp[0]][tmp[1]] + 1;
+                q.add(new int[]{tx, ty});
             }
+
         }
         return -1;
     }
