4088b8535dac7a97ba1fecbba4b934ab561158e0
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-12 10:29:51 +0900

    [Bronze V] Title: 합, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a ;
+		a = sc.nextInt();
+		int s = 0;
+		
+		for(int i = 1; i <=a; i++) {
+			s = s+i;
+		} System.out.print(s);
+	}
+		
+}
