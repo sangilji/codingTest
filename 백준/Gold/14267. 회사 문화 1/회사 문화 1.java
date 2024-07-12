3d8dff4c52e63b171acdba4cbc607f69345c1084
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-12 09:52:58 +0900

    [Gold IV] Title: 회사 문화 1, Time: 552 ms, Memory: 74556 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,82 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int MOD = 1_000_000_009;
-	static int INF = 2_000_000_000;
-	static int n;
-	static int m;
-	static int k;
-	static int x;
-	static int y;
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-	static int[] arr;
-	static List<Integer>[] list;
-	static List<Integer>[] graph;
-	static int[] tree;
-	static int[] depth;
-	static int[] parent;
-	static int[] visit;
-	static int[] top;
-	static int[] in;
-	static int[] out;
-	static int[] sz;
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
-	static int count = 0;
-	static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][] dp;
+    static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n + 1];
-		visit = new int[n + 1];
-		sz = new int[n + 1];
-		graph = new List[n + 1];
-		for (int i = 0; i <= n; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        sz = new int[n + 1];
 
-		}
-		for (int i = 2; i <=n ; i++) {
-			graph[arr[i]].add(i);
-		}
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			sz[a] += b;
-		}
-		dfs(1);
-		for (int i = 1; i <= n; i++) {
-			sb.append(sz[i]).append(" ");
-		}
-		System.out.println(sb);
+        list = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 2; i <=n; i++) {
+            list[arr[i]].add(i);
+        }
 
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sz[a] +=b;
+        }
+        dfs(1);
+        for (int i = 1; i <=n; i++) {
+            sb.append(sz[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
 
-	}
+    private static void dfs(int cur) {
+        for (int i = 0; i < list[cur].size(); i++) {
+            sz[list[cur].get(i)] += sz[cur];
+            dfs(list[cur].get(i));
+        }
+    }
 
-	private static void dfs(int cur) {
-		visit[cur]  = 1;
-		for (int i = 0; i < graph[cur].size(); i++) {
-			if (visit[graph[cur].get(i)] == 1) {
-				continue;
-			}
-			sz[graph[cur].get(i)] += sz[cur];
-			dfs(graph[cur].get(i));
-		}
-
-	}
 
 }
