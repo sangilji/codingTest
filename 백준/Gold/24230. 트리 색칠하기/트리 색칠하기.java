fd360a43a0f452e27632e59dc0a980fa44df0c56
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-16 10:51:54 +0900

    [Gold V] Title: 트리 색칠하기, Time: 1396 ms, Memory: 100760 KB -BaekjoonHub

---
+++ 백준/Gold/24230. 트리 색칠하기/트리 색칠하기.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        list = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+        visit = new int[n + 1];
+        System.out.println(dfs(1, 0));
+
+
+    }
+
+    private static int dfs(int cur, int color) {
+        visit[cur] = 1;
+        int tmp = 0;
+        if (arr[cur] != color) {
+            tmp = 1;
+        }
+        for (int i = 0; i < list[cur].size(); i++) {
+            if (visit[list[cur].get(i)] == 1) {
+                continue;
+            }
+            if (arr[cur] != color) {
+                tmp += dfs(list[cur].get(i), arr[cur]);
+            } else {
+                tmp += dfs(list[cur].get(i), color);
+
+            }
+        }
+        return tmp;
+    }
+
+
+}
