d084f3cdea6f9966ea9530763746632961178ec7
지상일 <88275989+sangilji@users.noreply.github.com>
2024-07-18 12:40:22 +0900

    [Gold V] Title: 강의실, Time: 876 ms, Memory: 56608 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[][] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
+
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        q.add(arr[0][1]);
+        for (int i = 1; i < n; i++) {
+            if (q.peek() > arr[i][0]) {
+                q.add(arr[i][1]);
+            }else{
+                q.poll();
+                q.add(arr[i][1]);
+            }
+
+        }
+        System.out.println(q.size());
+
+    }
+
+
+}
