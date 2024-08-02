f39b5aee77e327b1a1f96a8577e1f25ac9ed17f5
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-02 16:41:25 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int answer=0;
-		for(int i =0; i<5;i++){
-			answer += Math.pow(s.nextInt(),2);
-		}
-		System.out.println(answer%10);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int sum = ((a*a) + (b*b) + (c*c) + (d*d) + (e*e)) % 10;
+        System.out.println(sum);
+
+    }
 }
