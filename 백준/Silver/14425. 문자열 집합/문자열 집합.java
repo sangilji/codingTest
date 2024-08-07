db4e3a2f2ba9d55b34a152df6a4ae8c0a4b4b453
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-07 17:49:49 +0900

    [Silver IV] Title: 문자열 집합, Time: 416 ms, Memory: 31240 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,19 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        Map<String, Integer> map = new HashMap<>();
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(str.nextToken());
+        int m = Integer.parseInt(str.nextToken());
+
+        Map<String, String> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            map.put(br.readLine(), 0);
+            map.put(br.readLine()," ");
         }
         int count = 0;
         for (int i = 0; i < m; i++) {
-            if (map.containsKey(br.readLine())) count++;
+            String arr = br.readLine();
+            if(map.containsKey(arr)){
+                count++;
+            }
         }
-        System.out.print(count);
+        System.out.println(count);
+
     }
 }
