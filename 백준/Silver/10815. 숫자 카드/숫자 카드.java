28594b3ad1bcaab640ab13cdbd341f380804d156
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-08 14:31:53 +0900

    [Silver V] Title: 숫자 카드, Time: 2704 ms, Memory: 169144 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int n;
-	static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a; i++) {
+            int num1 = Integer.parseInt(st.nextToken());
+            map.put(num1, i);
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		Map<Integer, Boolean> map = new HashMap<>();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			map.put(Integer.parseInt(st.nextToken()), true);
-		}
-		int a = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < a; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if (map.getOrDefault(num, false)) {
-				sb.append("1 ");
-			} else {
-				sb.append("0 ");
-			}
-		}
-		System.out.println(sb);
-	}
+        int b = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            int num2 = Integer.parseInt(st.nextToken());
+            if (map.containsKey(num2)) {
+                System.out.print(1 + " ");
+            } else {
+                System.out.print(0 + " ");
+            }
+
+
+        }
+    }
 }
