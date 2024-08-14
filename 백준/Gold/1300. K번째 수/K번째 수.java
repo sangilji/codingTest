514f3c7c4f9ba9acc09e26c7ad779dc178b12e0c
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-14 16:57:44 +0900

    [Gold I] Title: K번째 수, Time: 156 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,31 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static long[] tree_min;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<int[]>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static int count = 0;
+    static int[][][][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
         long start = 1;
-        long last = Integer.MAX_VALUE;
-        long mid = 0;
-        while (start <= last) {
-            mid = (start + last) / 2;
-            long sum = 0;
-            for (int i = 1; i <= n; i++) {
-                long tmp = Math.min(mid, (long) i * n);
-                sum += tmp / i;
-            }
+        long end = (long) n * n;
+        long result = 0;
 
-            if (sum < k) {
-                start= mid+1;
+        while (start <= end) { // x보다 작은건 x개 미만, x랑 같거나 작은건 x개 이상 있어야함
+            long mid = (start + end) >> 1;
+            if (check(mid)) {
+                result = mid;
+                end = mid - 1;
             } else {
-                last = mid -1;
+                start = mid + 1;
             }
         }
-        System.out.println(start);
+        System.out.println(result);
+
     }
+
+    private static boolean check(long mid) {
+        long count = 0;
+        for (int i = 1; i <= n; i++) {
+            count += Math.min(mid / i, n);
+        }
+        return count >= m;
+    }
+
+
 }
