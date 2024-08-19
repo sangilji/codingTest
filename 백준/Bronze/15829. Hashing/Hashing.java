36c899eceb1633c1e024a81ccc002041ecb214b6
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-19 15:31:23 +0900

    [Bronze II] Title: Hashing, Time: 216 ms, Memory: 17736 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,9 +6,11 @@ public class Main {
 		int n = s.nextInt();
 		String a = s.next();
 		long result = 0;
+		long pow =1;
 		for (int i = 0; i < n; i++) {
 			int tmp = a.charAt(i) - 'a' + 1;
-			result = (long)((result + tmp * Math.pow(31, i))%1234567891);
+			result+= tmp*pow;
+			pow = (pow*31)%1234567891;
 		}
 		System.out.println(result % 1234567891);
 	}
