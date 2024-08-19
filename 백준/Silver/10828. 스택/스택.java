c7cea2e383d530bdea5da4966c596caa3ace4bfa
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-19 12:29:52 +0900

    [Silver IV] Title: 스택, Time: 316 ms, Memory: 19792 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,41 +1,48 @@
+import java.io.*;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n =s.nextInt();
-        int top = -1;
-        int[] st = new int[10000];
-        List<String> arr = new ArrayList<>();
-        for(int i =0;i<n;i++){
-            String tmp = s.next();
-            if(tmp.equals("push")){
-                arr.add(tmp);
-                arr.add(s.next());
-            } else {
-                arr.add(tmp);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int num = Integer.parseInt(br.readLine());
+
+        int temp = 0;
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <st.countTokens() ; j++) {
+                int count = stack.size();
+                String tempStr = st.nextToken();
+                if(tempStr.equals("push")){
+                    temp = Integer.parseInt((st.nextToken()));
+                    stack.add(temp);
+                } else if(tempStr.equals("pop")){
+                    if(count > 0){
+                        System.out.println(stack.pop());
+                    } else {
+                        System.out.println(-1);
+                    }
+                } else if (tempStr.equals("size")){
+                    System.out.println(stack.size());
+                } else if (tempStr.equals("empty")) {
+                    if(stack.empty()){
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                } else if(tempStr.equals("top")){
+                    if(count > 0){
+                        System.out.println(stack.peek());
+                    } else {
+                        System.out.println(-1);
+                    }
+                }
             }
-        }
-        int start= 0;
-        while(start!=arr.size()){
-            if(arr.get(start).equals("push")) {
-                st[++top]=Integer.parseInt(arr.get(++start));
-            } else if(arr.get(start).equals("pop")) {
-                System.out.println(top!=-1 ? st[top--] : -1);
-            } else if(arr.get(start).equals("size")) {
-                System.out.println(top+1);
-            } else if(arr.get(start).equals("empty")) {
-                System.out.println(top==-1 ? 1 : 0);
-            } else if(arr.get(start).equals("top")) {
-                System.out.println(top!=-1 ? st[top] : -1);
-            }
-            start++;
+
         }
     }
-
- 
 }
