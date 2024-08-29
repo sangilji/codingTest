0c32bd3cb54f3329712f534149c819117a32d8c8
지상일 <88275989+sangilji@users.noreply.github.com>
2024-08-29 12:00:40 +0900

    [Bronze III] Title: 덧셈과 곱셈, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/14579. 덧셈과 곱셈/덧셈과 곱셈.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static int[] tree;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        long result = 1;
+        for (int i = n; i <=m; i++) {
+            sum += i;
+            result *= sum;
+            result%=14579;
+        }
+        System.out.println(result);
+    }
+
+
+
+
+}
