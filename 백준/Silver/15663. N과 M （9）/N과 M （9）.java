8d99f626e37673f4742d827aedcbe1ba0f3658e3
지상일 <88275989+sangilji@users.noreply.github.com>
2024-09-02 12:59:55 +0900

    [Silver II] Title: N과 M (9), Time: 284 ms, Memory: 31500 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -2,45 +2,76 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.Set;
 
 public class Main {
-    static List<Integer> arr;
-    static boolean[] visit;
-    static LinkedHashSet<String> set = new LinkedHashSet<>();
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static int[] tree;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> s = new LinkedHashSet<>();
+
+
     public static void main(String[] args) throws IOException {
-        //코드를 작성하세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        visit = new boolean[n];
-        arr = new ArrayList<>();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            arr.add(tmp);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        arr.sort(Integer::compareTo);
-        dfs(0, m,"");
-        for (String s : set){
-            System.out.println(s);
+        visit = new int[n];
+        Arrays.sort(arr);
+        recur(0, "");
+        for (String string : s) {
+            sb.append(string).append("\n");
         }
+        System.out.println(sb);
+
     }
 
-    private static void dfs(int depth, int m, String s) {
+    private static void recur(int depth, String str) {
         if (depth == m) {
-            set.add(s);
+            s.add(str);
             return;
         }
-
-        for (int i = 0; i < arr.size(); i++) {
-            if (!visit[i]) {
-                visit[i]=true;
-                dfs( depth + 1, m, s+arr.get(i)+" ");
-                visit[i] =false;
+        for (int i = 0; i < n; i++) {
+            if (visit[i] == 1) {
+                continue;
             }
+            visit[i] = 1;
+            recur(depth + 1, str + arr[i] + " ");
+            visit[i] = 0;
         }
     }
 
-}
 
+}
