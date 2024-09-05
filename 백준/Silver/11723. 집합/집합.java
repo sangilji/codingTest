fb4a0e592863a67386d4e6a304bb0565abc74905
지상일 <88275989+sangilji@users.noreply.github.com>
2024-09-05 10:00:54 +0900

    [Silver V] Title: 집합, Time: 1000 ms, Memory: 313188 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,35 +1,69 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int bit = 0;
-        StringBuilder sb = new StringBuilder();
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
+
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static int[] tree;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> s = new LinkedHashSet<>();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[21];
         for (int i = 0; i < n; i++) {
-            String command = s.next();
-            if (command.equals("all")) {
-                bit |= ~0;
-                continue;
-            } else if (command.equals("empty")) {
-                bit &= 0;
-                continue;
-            }
-            int x = s.nextInt();
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
             if (command.equals("add")) {
-                bit |= (1 << (x - 1));
-            } else if (command.equals("check")) {
-                if ((bit & (1 << (x - 1))) != 0) {
-                    sb.append(1).append("\n");
-                } else {
-                    sb.append(0).append("\n");
-                }
+                int tmp = Integer.parseInt(st.nextToken());
+                arr[tmp] = 1;
             } else if (command.equals("remove")) {
-                bit = bit & ~(1 << (x - 1));
+                int tmp = Integer.parseInt(st.nextToken());
+                arr[tmp] = 0;
+            } else if (command.equals("check")) {
+                int tmp = Integer.parseInt(st.nextToken());
+                sb.append(arr[tmp]).append("\n");
             } else if (command.equals("toggle")) {
-                bit ^= (1 << (x - 1));
+                int tmp = Integer.parseInt(st.nextToken());
+                arr[tmp] = arr[tmp] == 1 ? 0 : 1;
+            } else if (command.equals("all")) {
+                Arrays.fill(arr, 1);
+            }else{
+                Arrays.fill(arr, 0);
             }
         }
         System.out.println(sb);
     }
+
+
 }
