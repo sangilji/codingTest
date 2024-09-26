e76e80ed04f56fa2e00bc1574feccfbe5dbed649
지상일 <88275989+sangilji@users.noreply.github.com>
2024-09-26 10:20:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,27 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int[] dp = new int[n + 1];
+    static int INF = 1_000_000_000;
+    static int MOD = 1_000_000_007;
+    static int n;
+    static int m;
+    static int k;
 
-        dp[3] = 1;
-        if (n >= 5) {
-            dp[5] = 1;
-        }
-        for (int i = 6; i <= n; i++) {
-            if (dp[i - 3] != 0) {
-                dp[i] = dp[i - 3] + 1;
+    static int[] arr;
+    static int[] arr2;
+    static int[] costs;
+    static int[] visit;
+    static int[] visit2;
+    static long[] sz;
+    static int[] depth;
+    static int[] parent;
+    static int[] top;
+    static int[] in;
+    static int[] out;
+    static int[] tree;
+    static long[] tree_max;
+    static List<Integer>[] list;
+    static List<Integer>[] graph;
+    static int[] dx = {1, 0, 1, 1};
+    static int[] dy = {1, 1, 0, -1};
+    static int[][] arr1;
+    static long count = 0;
+    static int[][] dp;
+    static int[][] dp2;
+    static StringBuilder sb = new StringBuilder();
+    static Set<Integer> s = new HashSet<>();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int min = INF;
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i <= 1000; i++) {
+            int current = n - i * 5;
+            if (current == 0) {
+                min = Math.min(min, i);
+                continue;
             }
-            if (dp[i - 5] != 0) {
-                dp[i] = dp[i - 5] + 1;
+            if (current < 0) {
+                break;
+            }
+            if (current % 3 == 0) {
+                min = Math.min(min, i + current / 3);
             }
         }
-        if (dp[n] == 0) {
+        if (min == INF) {
             System.out.println(-1);
             return;
         }
-        System.out.println(dp[n]);
+        System.out.println(min);
+
+
     }
+
+
 }
